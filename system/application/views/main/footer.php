d5d26174d9dddbddf309a895d687d59280fd69ac
Azhari Harahap <blackarazi@gmail.com>
2011-02-14 21:54:23 +0700

    Remove unused files from index

--- system/application/views/main/footer.php
+++
@@ -1,1 +0,0 @@
-Powered by <a style="text-decoration: underline;grm" href="#" id="about_button">Kalkun 0.2</a>
--- system/application/views/main/search.php
+++
@@ -1,8 +0,0 @@
-<?php 
-/*
-<?php echo form_open(); ?>
-<input type="text" name="search_message" />
-<input type="submit" value="Search Message" />
-<?php echo form_close(); ?>
-*/
-?>
