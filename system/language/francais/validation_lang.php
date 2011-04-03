9609d464a4de63f7a7d3b445af4000466418a2c5
ikerc <ikerc@notfear.net>
2011-04-04 00:39:54 +0200

    add FRENCH language in /kalkun/system/language/francais/

---
+++ system/language/francais/validation_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['required'] 			= "Le champ %s est requis.";
+$lang['isset']				= "Le champ %s doit contenir une valeur.";
+$lang['valid_email']		= "Le champ %s doit contenir un courriel valide.";
+$lang['valid_emails'] 		= "Le champ %s field must contain all valid email addresses.";
+$lang['valid_url'] 			= "Le champ %s doit contenir une URL valide.";
+$lang['valid_ip'] 			= "Le champ %s doit contenir une adresse IP valide.";
+$lang['min_length']			= "Le champ %s field must be at least %s characters in length.";
+$lang['max_length']			= "Le champ %s field can not exceed %s characters in length.";
+$lang['exact_length']		= "The %s field must be exactly %s characters in length.";
+$lang['alpha']				= "The %s field may only contain alphabetical characters.";
+$lang['alpha_numeric']		= "The %s field may only contain alpha-numeric characters.";
+$lang['alpha_dash']			= "The %s field may only contain alpha-numeric characters, underscores, and dashes.";
+$lang['numeric']			= "The %s field must contain a number.";
+$lang['is_numeric']			= "The %s field must contain a number.";
+$lang['integer']			= "The %s field must contain an integer.";
+$lang['matches']			= "The %s field does not match the %s field.";
+$lang['is_natural']			= "The %s field must contain a number.";
+$lang['is_natural_no_zero']	= "The %s field must contain a number greater than zero.";
+
+
+/* End of file validation_lang.php */
+/* Location: ./system/language/english/validation_lang.php */
