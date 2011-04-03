36fe1c187e885cf434f246b6c4d0130db79f8bcb
ikerc <ikerc@notfear.net>
2011-04-04 00:57:55 +0200

    profiler lang into french (partial)

--- system/language/francais/profiler_lang.php
+++ system/language/francais/profiler_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['profiler_database']		  = 'DATABASE';
-$lang['profiler_controller_info'] = 'CLASS/METHOD';
+$lang['profiler_database']		  = 'BESE DE DONNEE';
+$lang['profiler_controller_info'] = 'CLASSE/METHODE';
 $lang['profiler_benchmarks']	= 'BENCHMARKS';
 $lang['profiler_queries']		= 'QUERIES';
 $lang['profiler_get_data']		= 'GET DATA';
