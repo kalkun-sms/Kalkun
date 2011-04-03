7592c56ebeb5d801969ca29b1191ad926e3ea2a2
ikerc <ikerc@notfear.net>
2011-04-04 00:58:52 +0200

    add number lang into french

--- system/language/francais/number_lang.php
+++ system/language/francais/number_lang.php
@@ -1,10 +1,10 @@
 <?php
 
-$lang['terabyte_abbr'] = "TB";
-$lang['gigabyte_abbr'] = "GB";
-$lang['megabyte_abbr'] = "MB";
-$lang['kilobyte_abbr'] = "KB";
-$lang['bytes'] = "Bytes";
+$lang['terabyte_abbr'] = "TiB";
+$lang['gigabyte_abbr'] = "GiB";
+$lang['megabyte_abbr'] = "MiB";
+$lang['kilobyte_abbr'] = "KiB";
+$lang['bytes'] = "Octets";
 
 /* End of file number_lang.php */
 /* Location: ./system/language/english/number_lang.php */
