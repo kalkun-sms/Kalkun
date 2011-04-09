a698b860f0ac99d9e4c65826bdad12c913dcb842
Kinshuk Bairagi <kinshuk1989@gmail.com>
2011-04-10 01:39:58 +0530

    Search box corner rounded

--- application/views/main/search.php
+++ application/views/main/search.php
@@ -2,7 +2,7 @@
 <?php echo form_open("messages/search/results/all", array('class' => 'sms_search_form')); ?>
 <table border="0" cellpadding="0" cellspacing="0">
 <tr valign="top">
-<td><input type="text" name="search_sms" value="<?php if (isset($search_string)) echo $search_string;?>" /></td>
+<td><input type="text" name="search_sms" value="<?php if (isset($search_string)) echo $search_string;?>"  class="ui-corner-all"/></td>
 <td><input type="submit" value="Search Message" /></td>
 </tr>
 </table>
