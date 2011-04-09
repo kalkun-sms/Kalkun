d82676a488767910a987e6856fe12cf6e53c7a25
Kinshuk Bairagi <kinshuk1989@gmail.com>
2011-04-10 01:38:19 +0530

    Fixed CSS bug for contact search

--- application/views/main/search.php
+++ application/views/main/search.php
@@ -1,5 +1,5 @@
 <?php if($this->uri->segment(1) == 'messages'): ?>
-<?php echo form_open("messages/search/results/all", array('class' => 'search_form')); ?>
+<?php echo form_open("messages/search/results/all", array('class' => 'sms_search_form')); ?>
 <table border="0" cellpadding="0" cellspacing="0">
 <tr valign="top">
 <td><input type="text" name="search_sms" value="<?php if (isset($search_string)) echo $search_string;?>" /></td>
