0672682e04686073e4156efb7d388cc83bb2a50d
Azhari Harahap <blackarazi@gmail.com>
2011-04-11 00:35:02 +0700

    Search box layout css fix, tested with FF3, Chrome, and Safari

--- application/views/main/search.php
+++ application/views/main/search.php
@@ -1,7 +1,8 @@
 <?php echo form_open("messages/search/results/all", array('class' => 'sms_search_form')); ?>
 <table border="0" cellpadding="0" cellspacing="0">
 <tr valign="top">
-<td><input type="text" name="search_sms" value="<?php if (isset($search_string)) echo $search_string;?>" class="ui-corner-left"  /> <input type="submit" value="Search Message" /></td>
+<td><input type="text" name="search_sms" value="<?php if (isset($search_string)) echo $search_string;?>" class="ui-corner-left" /></td> 
+<td><input type="submit" value="Search Message" /></td>
 </tr>
 </table>
 <?php echo form_close(); ?>
