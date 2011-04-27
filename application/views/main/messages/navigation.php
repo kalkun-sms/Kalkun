487516c9324bb40a9a716806cce3cb2f32474b17
Kinshuk Bairagi <kinshuk1989@gmail.com>
2011-04-28 03:45:31 +0530

    Fixed Page Movement on button click.

--- application/views/main/messages/navigation.php
+++ application/views/main/messages/navigation.php
@@ -24,8 +24,8 @@
 		<li>&nbsp;</li>
 		<?php endif;?>
 	<?php endif;?>
-	<li><a href="#" class="select_all_button button"><?php echo lang('kalkun_select_all');?></a></li>
-	<li><a href="#" class="clear_all_button button"><?php echo lang('kalkun_clear_all');?></a></li>
+	<li><a href="javascript:void(0);" class="select_all_button button"><?php echo lang('kalkun_select_all');?></a></li>
+	<li><a href="javascript:void(0);" class="clear_all_button button"><?php echo lang('kalkun_clear_all');?></a></li>
 	<?php 
 	if($this->uri->segment(2)=='folder' && $this->uri->segment(3)=='outbox'): 
 	elseif($this->uri->segment(2)=='conversation' && $this->uri->segment(4)=='outbox'):
@@ -33,12 +33,12 @@
 	<li>&nbsp;</li>
 	<li><a class="move_to_button button" href="#"><?php echo lang('kalkun_move_to');?></a></li>	
 	<?php endif; ?>
-	<li><a class="global_delete button" href="#">
+	<li><a class="global_delete button" href="javascript:void(0);">
 	<?php echo lang('kalkun_delete'); 
 	if($this->uri->segment(4)=='5' or $this->uri->segment(6)=='5') echo " ".lang('kalkun_permanently');?></a></li>	
 	<?php if($this->uri->segment(2) != 'search'): ?>
 	<li>&nbsp;</li>
-	<li><a href="#" class="refresh_button button"><?php echo lang('kalkun_refresh');?></a></li>	
+	<li><a href="javascript:void(0);" class="refresh_button button"><?php echo lang('kalkun_refresh');?></a></li>	
     <?php endif; ?>		
 	<?php if($this->uri->segment(2) != 'search' && $this->pagination->create_links()!=''): ?>
 	<li class="paging"><div id="paging"><?php  echo $this->pagination->create_links();?></div></li>
