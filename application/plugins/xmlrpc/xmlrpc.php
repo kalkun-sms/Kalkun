e7bd0e0ad18fdb3d1ec6a612f5d1d9328baaeee0
Azhari Harahap <blackarazi@gmail.com>
2012-07-07 23:29:13 +0700

    Changes: add simple xmlrpc server plugin for sending sms (Issue #18)

---
+++ application/plugins/xmlrpc/xmlrpc.php
@@ -0,0 +1,49 @@
+<?php
+/**
+* Plugin Name: XMLRPC
+* Plugin URI: http://azhari.harahap.us
+* Version: 0.1
+* Description: XMLRPC Server Plugin
+* Author: Azhari Harahap
+* Author URI: http://azhari.harahap.us
+*/
+
+
+/**
+* Function called when plugin first activated
+* Utility function must be prefixed with the plugin name
+* followed by an underscore.
+* 
+* Format: pluginname_activate
+* 
+*/
+function xmlrpc_activate()
+{
+    return true;
+}
+
+/**
+* Function called when plugin deactivated
+* Utility function must be prefixed with the plugin name
+* followed by an underscore.
+* 
+* Format: pluginname_deactivate
+* 
+*/
+function xmlrpc_deactivate()
+{
+    return true;
+}
+
+/**
+* Function called when plugin first installed into the database
+* Utility function must be prefixed with the plugin name
+* followed by an underscore.
+* 
+* Format: pluginname_install
+* 
+*/
+function xmlrpc_install()
+{
+    return true;
+}
