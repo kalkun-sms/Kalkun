d65d0a3763a1bf13f0064e1f068f32b165d79ce4
Azhari Harahap <blackarazi@gmail.com>
2012-07-28 20:45:05 +0700

    Released: Version 0.6

--- application/config/kalkun_settings.php
+++ application/config/kalkun_settings.php
@@ -6,11 +6,11 @@
 |--------------------------------------------------------------------------
 |
 */
-$config['kalkun_version'] = '0.5';
-$config['kalkun_codename'] = 'Ubud';
-$config['kalkun_release_date'] = '1 May 2012';
-$config['kalkun_upgradeable'] = FALSE;
-$config['kalkun_previous_version'] = '0.4';
+$config['kalkun_version'] = '0.6';
+$config['kalkun_codename'] = 'Wakatobi';
+$config['kalkun_release_date'] = '28 July 2012';
+$config['kalkun_upgradeable'] = TRUE;
+$config['kalkun_previous_version'] = '0.5';
 
 /*
 |--------------------------------------------------------------------------
