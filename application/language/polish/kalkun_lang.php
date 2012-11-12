86d9ca38060095c496eb6dd66f364f0dbc8be9a4
Mariusz Bialonczyk <manio@skyboo.net>
2012-11-12 13:36:54 +0100

    Added Polish translation

---
+++ application/language/polish/kalkun_lang.php
@@ -0,0 +1,340 @@
+<?php
+// Polish Translation
+// Author: Mariusz Białończyk <manio@skyboo.net>
+
+// Global
+$lang['kalkun_title'] = "Kalkun: System zarządzania SMS przez WWW";
+$lang['kalkun_dashboard'] = "Strona główna";
+$lang['kalkun_folder'] = "Foldery";
+$lang['kalkun_myfolder'] = "Moje Foldery";
+
+$lang['kalkun_compose'] = "Nowa wiadomość";
+$lang['kalkun_inbox'] = "Skrzynka odbiorcza";
+$lang['kalkun_outbox'] = "Skrzynka nadawcza";
+$lang['kalkun_sentitems'] = "Elementy wysłane";
+$lang['kalkun_senterroritems'] = "Błędy wysyłania";
+$lang['kalkun_trash'] = "Kosz";
+$lang['kalkun_spam'] = "Spam";
+$lang['kalkun_add'] = "Dodaj";
+$lang['kalkun_action'] = "Akcje";
+
+$lang['kalkun_phonebook'] = "Książka adresowa";
+$lang['kalkun_setting'] = "Ustawienia";
+$lang['kalkun_about'] = "O systemie";
+$lang['kalkun_logout'] = "Wyloguj";
+
+$lang['kalkun_year'] = "rok";
+$lang['kalkun_month'] = "miesiąc";
+$lang['kalkun_week'] = "tydzień";
+$lang['kalkun_day'] = "dzień";
+$lang['kalkun_hour'] = "godzina";
+$lang['kalkun_minute'] = "minuta";
+$lang['kalkun_second'] = "sekunda";
+$lang['kalkun_ago'] = "temu";
+
+// Dashboard
+$lang['kalkun_notification'] = "Powiadomienia";
+$lang['kalkun_statistic'] = "Statystyki";
+$lang['kalkun_system_information'] = "Informacje o systemie";
+$lang['kalkun_alerts'] = "Alerty";
+$lang['kalkun_operating_system'] = "System operacyjny";
+$lang['kalkun_gammu_version'] = "Wersja Gammu";
+$lang['kalkun_gammu_db_schema'] = "Wersja bazy Gammu";
+$lang['kalkun_phone_imei'] = "Numer IMEI";
+$lang['kalkun_you_have'] = "Masz";
+$lang['kalkun_new_message'] = "wiadomości";
+$lang['kalkun_last_phone_activity'] = "Ostatnia aktywność modemu";
+$lang['kalkun_battery'] = "Poziom baterii";
+$lang['kalkun_signal'] = "Sygnał";
+
+// Phonebook
+$lang['kalkun_contact'] = "Kontakt";
+$lang['kalkun_group'] = "Grupy";
+$lang['kalkun_add_to_group'] = "Dodaj do grupy";
+$lang['kalkun_delete_from_group'] = "Usuń z grupy";
+$lang['kalkun_pbk_add_method'] = "Wybierz metodę dodawania";
+$lang['kalkun_pbk_add_form'] = "Użycie formularza";
+$lang['kalkun_pbk_add_form_desc'] = "Ręczne dodanie osoby przez formularz";
+$lang['kalkun_pbk_add_csv'] = "Import z pliku CSV";
+$lang['kalkun_pbk_add_csv_desc'] = "Importuje kontakt z pliku o formacie CSV";
+$lang['kalkun_pbk_add_csv_file'] = "Plik CSV";
+$lang['kalkun_pbk_add_csv_label'] = "Plik CSV musi mieć właściwy format";
+$lang['kalkun_pbk_del_contact_conf'] = "Czy jesteś pewien, że chcesz usunąć <span id='contact-delete-count'></span> kontaktów?";
+$lang['kalkun_valid_example'] = "Właściwy przykład";
+
+// Message
+$lang['kalkun_message'] = "Wiadomość";
+$lang['kalkun_send'] = "Wyślij";
+$lang['kalkun_send_to'] = "Wyślij do";
+$lang['kalkun_reply'] = "Odpowiedź";
+$lang['kalkun_reply_to'] = "Odpowiedź do";
+$lang['kalkun_reply_title'] = "Kliknij, aby wysłać szybką odpowiedź";
+$lang['kalkun_quick_reply'] = "Szybka odpowiedź";
+$lang['kalkun_quick_reply_title'] = "Kliknij, aby wysłać szybką odpowiedź";
+$lang['kalkun_forward'] = "Prześlij dalej";
+$lang['kalkun_forward_to'] = "Prześlij dalej do";
+$lang['kalkun_forward_title'] = "Kliknij, aby przesłać tę wiadomość dalej";
+$lang['kalkun_move_to'] = "Przenieś do";
+$lang['kalkun_select_folder'] = "Wybierz folder";
+$lang['kalkun_delete'] = "Usuń";
+$lang['kalkun_delete_message_title'] = "Kliknij, aby usunąć tę wiadomość";
+$lang['kalkun_delete_folder'] = "Usuń ten folder";
+$lang['kalkun_delete_folder_title'] = "Kliknij, aby usunąć ten folder";
+$lang['kalkun_permanently'] = "trwale";
+$lang['kalkun_recover'] = "Odzyskaj";
+
+$lang['kalkun_send_date'] = "Data wysłania";
+$lang['kalkun_sms_mode'] = "Tryb SMS";
+$lang['kalkun_now'] = "Teraz";
+$lang['kalkun_now_note'] = "Wiadomość zostanie wysłana natychmiast";
+$lang['kalkun_at_date_time'] = "O określonym czasie";
+$lang['kalkun_after_a_delay'] = "Po określonym czasie";
+
+$lang['kalkun_person'] = "Osoba";
+$lang['kalkun_group'] = "Grupa";
+$lang['kalkun_input_manually'] = "Dodaj ręcznie";
+
+$lang['kalkun_show_hide_option'] = "Pokaż/ukryj opcje";
+$lang['kalkun_add_folder'] = "Dodaj folder";
+$lang['kalkun_folder_name'] = "Nazwa folderu";
+$lang['kalkun_cancel'] = "Anuluj";
+$lang['kalkun_save'] = "Zapisz";
+$lang['kalkun_rename'] = "Zmień nazwę";
+$lang['kalkun_rename_folder'] = "Zmień nazwę folderu";
+$lang['kalkun_rename_folder_title'] = "Kliknij, aby zmienić nazwę folderu";
+
+$lang['kalkun_delete_folder_confirmation_header'] = "Potwierdź usunięcie folderu";
+$lang['kalkun_delete_folder_confirmation'] = "Folder wraz z wiadomościami zostanie trwale usunięty. Czy jesteś pewien?";
+
+$lang['kalkun_no_message'] = "Brak wiadomości w ";
+$lang['kalkun_no_message_in_folder'] = "Brak wiadomości w tym folderze";
+
+$lang['kalkun_delete_all_confirmation_header'] = "Skasuj Wszystkie";
+$lang['kalkun_delete_all_confirmation'] = "Jesteś pewien? Skasuje to wszystkie konwersacje.";
+
+$lang['kalkun_select_all'] = "Zaznacz wszystko";
+$lang['kalkun_select_all_title'] = "Kliknij, aby zaznaczyć całą wiadomość";
+$lang['kalkun_clear_all'] = "Wyczyść zaznaczenie";
+$lang['kalkun_clear_all_title'] = "Kliknij, aby wyczyścić zaznaczenie wszystkich wiadomości";
+$lang['kalkun_refresh'] = "Odśwież";
+$lang['kalkun_global_delete_title'] = "Kliknij, aby skasować wybraną wiadomość";
+
+$lang['kalkun_report_spam'] = "Oznacz jako Spam";
+$lang['kalkun_ham'] = "Nie Spam";
+
+$lang['kalkun_first'] = "Pierwszy";
+$lang['kalkun_last'] = "Ostatni";
+
+$lang['kalkun_smsc'] = "SMSC";
+$lang['kalkun_sms_part'] = "Część";
+$lang['kalkun_sms_part_suffix'] = "część wiadomości";
+$lang['kalkun_sms_validity'] = "Ważność";
+
+$lang['kalkun_sms_type'] = "Typ SMS-a";
+$lang['kalkun_sms_type_normal'] = "Zwykły";
+$lang['kalkun_sms_type_flash'] = "Flash";
+$lang['kalkun_sms_type_wap'] = "Link WAP Push";
+$lang['kalkun_sms_type_wap_url'] = "URL";
+
+$lang['kalkun_sms_ads_active'] = "Reklamy aktywne";
+$lang['kalkun_sms_ncpr_check'] = "Sprawdź DND";
+$lang['kalkun_insert'] = "Wstaw";
+
+$lang['kalkun_canned'] = "Odpowiedź automatyczna";
+$lang['kalkun_canned_empty'] = "Wygląda na to, że nie ma jeszcze zdefiniowanych odpowiedzi automatycznych. Najpierw utwórz jakieś";
+$lang['kalkun_canned_title'] = "Wybierz odpowiedź";
+$lang['kalkun_canned_save_new'] = "Zapisz jako nową...";
+$lang['kalkun_canned_saving'] = "Zapisywanie...";
+$lang['kalkun_canned_confirm'] = "Czy jesteś pewien?";
+$lang['kalkun_canned_template_name'] = "Podaj nazwę dla tego szablonu. Nazwa powinna być unikalna.";
+$lang['kalkun_canned_template_update'] = "Czy jesteś pewien?  Poprzedni szablon zostanie nadpisany";
+
+// Others
+$lang['kalkun_compose_amount'] = "Ilość";
+$lang['kalkun_compose_send_as_flash'] = "Wyślij jako Flash SMS";
+$lang['kalkun_compose_send_as_unicode'] = "Wyślij w Unicode";
+$lang['kalkun_compose_times'] = "razy";
+$lang['kalkun_compose_hour'] = "Godzina";
+$lang['kalkun_compose_minutes'] = "Minuty";
+$lang['kalkun_no_message_search'] = "Twoje zapytanie nie zwróciło wyników!";
+$lang['tni_see_conversation'] = "Zobacz konwersację";
+
+$lang['kalkun_search'] = "Szukaj";
+$lang['kalkun_advanced_search'] = "Wyszukiwanie zaawansowane";
+$lang['kalkun_advanced_search_from_to'] = "Od/Do";
+$lang['kalkun_advanced_search_query'] = "Treść";
+$lang['kalkun_advanced_search_on'] = "Wyszukiwanie Włączone";
+$lang['kalkun_advanced_search_on_all_folder'] = "Wszystkie Foldery";
+$lang['kalkun_advanced_search_date_from'] = "Data Od";
+$lang['kalkun_advanced_search_date_to'] = "Data Do";
+$lang['kalkun_advanced_search_msg_status'] = "Stan";
+$lang['kalkun_advanced_search_msg_status_any'] = "Dowolny";
+$lang['kalkun_advanced_search_paging'] = "Stronicowanie";
+$lang['kalkun_advanced_search_paging_per_page'] = "Na stronę";
+$lang['kalkun_advanced_search_no_paging'] = "Nie stronicuj";
+
+$lang['kalkun_resend'] = "Wyślij ponownie";
+$lang['kalkun_public_contact'] = "Kontakt publiczny";
+$lang['kalkun_public_contact_set'] = "Ustaw jako kontakt publiczny";
+$lang['kalkun_my_contact'] = "Moje Kontakty";
+$lang['kalkun_public_group'] = "Publiczne Grupy";
+$lang['kalkun_public_group_set'] = "Ustaw jako Grupę Publiczną";
+$lang['kalkun_my_group'] = "Moje Grupy";
+$lang['kalkun_statistic_title'] = "SMS-y w ciągu ostatnich 7 dni";
+$lang['kalkun_incoming_sms'] = "Przychodzące SMS-y";
+$lang['kalkun_outgoing_sms'] = "Wychodzące SMS-y";
+$lang['kalkun_delete_all_message_now'] = "Skasuj wszystkie wiadomości";
+$lang['kalkun_contact_del_title'] = "Potwierdzenie usunięcia kontaktów";
+$lang['kalkun_compose_valid_url'] = "Powinien być prawidłowym URL";
+$lang['kalkun_compose_import_file'] = "Import z pliku";
+$lang['kalkun_compose_counter_character'] = "znaków";
+$lang['kalkun_compose_counter_message'] = "wiadomości";
+
+// Setting
+$lang['kalkun_setting_passwd_current'] = "Aktualne hasło";
+$lang['kalkun_setting_passwd_forgot'] = "Zapomniałem hasła";
+$lang['kalkun_setting_passwd_new'] = "Nowe Hasło";
+$lang['kalkun_setting_passwd_valid_rule'] = "Musi mieć przynajmniej 6 znaków";
+
+$lang['kalkun_setting_passwd_current_enter'] = "Podaj aktualne hasło";
+$lang['kalkun_setting_passwd_new_enter'] = "Podaj nowe hasło";
+
+$lang['kalkun_level_admin'] = "Administrator";
+$lang['kalkun_level_user'] = "Użytkownik";
+
+// SMS Content/Member
+$lang['kalkun_sms_member'] = "Członek";
+$lang['kalkun_sms_total_member'] = "Ilość członków";
+$lang['kalkun_sms_no_member'] = "Brak zarejestrowanych członków";
+
+// tni contribution
+$lang['tni_contact_add'] = "Dodaj kontakt";
+$lang['tni_send_message'] = "Wyślij wiadomość";
+$lang['tni_sending_message'] = "Wysyłanie";
+$lang['tni_folders_sms'] = "Foldery SMS";
+$lang['tni_name_search'] = "Wpisz nazwę z grup kontaktów";
+$lang['tni_group_del_title'] = "Potwierdzenie usunięcia grup(y)";
+$lang['tni_group_delete'] = "Usuwanie grup(y)?<br>Wszystkie kontakty z tej grup(y) zostaną także usunięte.";
+$lang['tni_group_del_button'] = "Tak, usuń wybraną grupę(y).";
+$lang['tni_group_add'] = "Utwórz grupę";
+$lang['tni_group_name'] = "Nazwa grupy";
+$lang['tni_group_show_contacts'] = "Pokaż kontakty z tej grupy";
+$lang['tni_group_field_num'] = "Ilość pól";
+$lang['tni_group_field_string'] = "Wartości pól";
+$lang['tni_group_field_title'] = "Group SMS Field Values";
+$lang['tni_groups'] = "Grupy";
+$lang['tni_groups_manage'] = "Zarządzaj grupami";
+$lang['tni_group_manage'] = "Zarządzaj grupą";
+$lang['tni_groups_edit_contacts'] = "Edytuj kontakt";
+$lang['tni_group_select'] = "Wybierz grupę";
+$lang['tni_msglist_trash_empty'] = "Kosz jest pusty";
+$lang['tni_msg_no_conv_selected'] = "Nie wybrano pozycji";
+$lang['tni_pbk_no_contact_selected'] = "Nie wybrano kontaktów";
+$lang['tni_compose_sms'] = "Tworzenie SMS-a";
+$lang['tni_contacts'] = "Kontakty";
+$lang['tni_settings_saved'] = "Twoje ustawienia zostały zapisane...";
+$lang['tni_settings_wrong_pword'] = "Podałeś złe hasło";
+$lang['tni_settings_uname_exists'] = "Ten użytkownik już istnieje";
+$lang['tni_settings_unamepass_wrong'] = "Niepoprawna nazwa użytkownika lub hasło";
+$lang['tni_back_to'] = "Wróć do";
+$lang['tni_show_details'] = "Pokaż szczegóły";
+$lang['tni_hide_details'] = "Ukryj szczegóły";
+$lang['tni_search_contacts'] = "Szukaj kontaktów";
+$lang['tni_search_sms'] =  "Szukaj wiadomości";
+$lang['tni_edit'] = "Edycja";
+$lang['tni_form_fields_required'] = "Wszystkie pola są wymagane";
+$lang['tni_contact_name'] = "Nazwa";
+$lang['tni_contact_phonenumber'] = "Numer telefonu";
+$lang['tni_compose_msgto_outbox'] = "Wiadomość została przeniesiona do skrzynki nadawczej<br>i jest gotowa do dostarczenia.";
+$lang['tni_email_address'] = "Adres e-mail";
+$lang['tni_email_forward'] = "Załącz przekierowanie na e-mail";
+$lang['tni_compose_searching'] = "Wyszukiwanie...";
+$lang['tni_outbox_sent_time'] = "Mniej niż minuta";
+$lang['tni_time_remaining'] = "pozostało";
+$lang['tni_pbk_edit_contact'] = "Edytuj kontakt";
+$lang['tni_pbk_contact_edited'] = "Kontakt poprawiony";
+$lang['tni_pbk_contact_added'] = "Kontakt dodany";
+$lang['tni_contact_not_found'] = "Nie znaleziono kontaktów";
+$lang['tni_contact_search_empty'] = "Brak kontaktów";
+$lang['tni_user_add'] = "Dodaj/Edytuj Użytkowników";
+$lang['tni_user_addp'] = "Dodaj użytkowników";
+$lang['tni_user_edit'] = "Edytuj/Dodaj użytkownika";
+$lang['tni_user_word'] = "Użytkownik";
+$lang['tni_user_wordp'] = "Użytkownicy";
+$lang['tni_user_confirm_delete'] = "Potwierdzenie usunięcia użytkownika";
+$lang['tni_user_delete_body'] = "Usunąć użytkownika?<br>Usunięcie spowoduje także usunięcie jego danych (wiadomości i kontaktów).";
+$lang['tni_user_not_found'] = "Nie znaleziono użytkownika";
+$lang['tni_user_search_empty'] = "Puste wyszukiwanie";
+$lang['tni_user_person_name'] = "Nazwa";
+$lang['tni_user_username'] = "Nazwa użytkownika";
+$lang['tni_user_password'] = "Hasło";
+$lang['tni_user_search'] = "Wyszukaj użytkownika";
+$lang['tni_user_conf_password'] = "Powtórz hasło";
+$lang['tni_user_updated'] = "Użytkownik zaktualizowany";
+$lang['tni_user_phone_number'] = "Numer telefonu";
+$lang['tni_action_not_allowed'] = "Akcja niedozwolona";
+$lang['tni_error_enter_name'] = "Podaj nazwę";
+$lang['tni_error_enter_password'] = "Podaj hasło";
+$lang['tni_error_password_nomatch'] = "Hasła się nie zgadzają";
+$lang['tni_error_toshort'] = "Za krótkie";
+$lang['tni_error_nouser_sel'] = "Nie wybrano użytkownika";
+$lang['tni_level'] = "Poziom uprawnień";
+$lang['tni_delete_folder'] = "Skasuj ten folder";
+$lang['tni_loading'] = "Ładowanie baterii";
+$lang['tni_status'] = "Stan";
+$lang['tni_connected'] = "Połączony";
+$lang['tni_disconnected'] = "Rozłączony";
+$lang['tni_user_enter_login'] = "Proszę podać nazwę użytkownika i hasło";
+$lang['tni_group_no_group'] = "Brak zdefiniowanych grup, musisz najpierw utworzyć grupę";
+$lang['tni_group_no_selected'] = "Brak wybranej grupy";
+$lang['tni_original_msg_from'] = "Od";
+$lang['tni_compose_enter_dest'] = "Podaj numer docelowy";
+$lang['tni_compose_enter_msg'] = "Podaj treść wiadomości";
+$lang['tni_compose_enter_sendate'] = "Podaj datę wysłania";
+$lang['tni_compose_click_calendar'] = "Kliknij na kalendarz";
+$lang['tni_archived_by'] = "Show by";
+$lang['tni_received'] = "Odebrano";
+$lang['tni_from'] = "Od";
+$lang['tni_to'] = "Do";
+$lang['tni_inserted'] = "Wstawiono";
+$lang['tni_date'] = "Data";
+$lang['tni_msg_stat_fail'] = "Błąd wysyłania";
+$lang['tni_msg_stat_oknr'] = "Wysłano poprawnie - Bez Raportu";
+$lang['tni_msg_stat_okwr'] = "Wysłano poprawnie - Oczekuję na Raport";
+$lang['tni_msg_stat_deliv'] = "Dostarczono";
+$lang['tni_msg_stat_pend'] = "Oczekuje";
+$lang['tni_msg_stat_unknown'] = "Nieznany";
+
+$lang['tni_set_conv_order_old'] = "Najpierw najstarsze";
+$lang['tni_set_conv_order_new'] = "Najpierw najnowsze";
+$lang['tni_set_conv_sort'] = "Typ sortowania";
+
+$lang['tni_set_data_pp'] = "Ilość danych na stronę";
+$lang['tni_set_data_pp_hint'] = "Użyte w stronicowaniu wiadomości i książki adresowej";
+
+$lang['tni_set_perm_del'] = "Trwałe usuwanie";
+$lang['tni_set_perm_deloff'] = "Trwałe usuwanie wyłączone";
+$lang['tni_set_perm_deloff_hint'] = " - Zawsze najpierw usuwaj do kosza";
+$lang['tni_set_perm_delon'] = "Trwałe usuwanie załączone";
+
+$lang['tni_set_title'] = "Ustawienia użytkownika";
+$lang['tni_set_general'] = "Ogólne";
+$lang['tni_set_personal'] = "Osobiste";
+$lang['tni_set_appearance'] = "Wygląd";
+
+
+$lang['tni_signature']  = "Sygnatura";
+$lang['tni_signature_off']  = "Sygnatura załączona";
+$lang['tni_signature_on']  = "Sygnatura wyłączona";
+$lang['tni_signature_hint']  = "Maks. 50 znaków";
+$lang['tni_signature_hintb']  = "Sygnatura będzie dodawana na końcu wiadomości SMS";
+$lang['tni_language']  = "Język";
+$lang['tni_settings']  = "Ustawienia";
+$lang['tni_yes']  = "Tak";
+$lang['tni_no'] = "Nie";
+$lang['tni_default'] = "Domyślnie";
+$lang['tni_set_deliv_report'] = "Raport doręczenia";
+
+
+/* End of file kalkun_lang.php */
+/* Location: ./application/language/polish/kalkun_lang.php */
