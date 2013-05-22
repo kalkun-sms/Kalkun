e44c0f66b3c76c45377664b8bd8cbf9d5c925fd4
Azhari Harahap <blackarazi@gmail.com>
2013-05-23 00:46:53 +0700

    added hungarian translation

---
+++ application/language/hungarian/kalkun_lang.php
@@ -0,0 +1,346 @@
+<?php
+// Hungarian Translation
+// Created by: Nándor Simon <simon.nandor@simonszoft.hu>
+
+// Global
+$lang['kalkun_title'] = "tmi: Web based SMS Management";
+$lang['kalkun_dashboard'] = "Irányítópult";
+$lang['kalkun_folder'] = "Mappák";
+$lang['kalkun_myfolder'] = "Saját mappák";
+
+$lang['kalkun_compose'] = "Létrehozás";
+$lang['kalkun_inbox'] = "Beérkezett üzenetek";
+$lang['kalkun_outbox'] = "Elküldött üzenetek";
+$lang['kalkun_sentitems'] = "Elküldött elemek";
+$lang['kalkun_senterroritems'] = "Küldési hiba";
+$lang['kalkun_trash'] = "Kuka";
+$lang['kalkun_spam'] = "Spam";
+$lang['kalkun_add'] = "Hozzáadás";
+$lang['kalkun_action'] = "Mûvelet";
+
+$lang['kalkun_phonebook'] = "Telefonkönyv";
+$lang['kalkun_setting'] = "Beállítások";
+$lang['kalkun_filters'] = "Szûrõk";
+$lang['kalkun_about'] = "Névjegy";
+$lang['kalkun_logout'] = "Kilépés";
+
+$lang['kalkun_year'] = "év";
+$lang['kalkun_month'] = "hónap";
+$lang['kalkun_week'] = "hét";
+$lang['kalkun_day'] = "nap";
+$lang['kalkun_hour'] = "óra";
+$lang['kalkun_minute'] = "perc";
+$lang['kalkun_second'] = "másodperc";
+$lang['kalkun_ago'] = "ezelõtt";
+
+// Dashboard
+$lang['kalkun_notification'] = "Figyelmeztetés";
+$lang['kalkun_statistic'] = "Statisztikák";
+$lang['kalkun_system_information'] = "Rendszer információk";
+$lang['kalkun_alerts'] = "Riasztások";
+$lang['kalkun_operating_system'] = "Operációs rendszer";
+$lang['kalkun_gammu_version'] = "Gammu Verzió";
+$lang['kalkun_gammu_db_schema'] = "Gammu DB séma";
+$lang['kalkun_phone_imei'] = "Modem IMEI";
+$lang['kalkun_you_have'] = "Önnek";
+$lang['kalkun_new_message'] = "új üzenete van";
+$lang['kalkun_last_phone_activity'] = "Utolsó modemaktivitás";
+$lang['kalkun_battery'] = "Akkumlátor";
+$lang['kalkun_signal'] = "Jel";
+
+// Phonebook
+$lang['kalkun_contact'] = "Kapcsolat";
+$lang['kalkun_all_contacts'] = "Összes kapcsolat";
+$lang['kalkun_send_to_all_contacts'] = "Küldés mindegyik kapcsolatnak";
+$lang['kalkun_group'] = "Csoportok";
+$lang['kalkun_add_to_group'] = "Hozzáadás csoporthoz";
+$lang['kalkun_delete_from_group'] = "Törlés csoportból";
+$lang['kalkun_pbk_add_method'] = "Kapcsolat hozzáadásának kiválasztása";
+$lang['kalkun_pbk_add_form'] = "Kapcsolati ûrlap használata";
+$lang['kalkun_pbk_add_form_desc'] = "Kapcsolat hozzáadása manuálisan ûrlap segítségével";
+$lang['kalkun_pbk_add_csv'] = "CSV fájl importálása";
+$lang['kalkun_pbk_add_csv_desc'] = "Kapcsolatok importálása formázott CSV fájlból";
+$lang['kalkun_pbk_add_csv_file'] = "CSV fájl";
+$lang['kalkun_pbk_add_csv_label'] = "A CSV fájl formátumának helyesnek kell lennie";
+$lang['kalkun_pbk_del_contact_conf'] = "Biztosan törölni szeretnéd a <span id='contact-delete-count'></span> kapcsolato(ka)t?";
+$lang['kalkun_valid_example'] = "Érvényes példa";
+
+// Message
+$lang['kalkun_message'] = "Üzenet";
+$lang['kalkun_send'] = "Elküld";
+$lang['kalkun_send_to'] = "Küldés";
+$lang['kalkun_reply'] = "Válasz";
+$lang['kalkun_reply_to'] = "Válasz";
+$lang['kalkun_reply_title'] = "Kattintson az üzenetre való válaszoláshoz";
+$lang['kalkun_quick_reply'] = "Gyors válasz";
+$lang['kalkun_quick_reply_title'] = "Kattintson ha gyors módban szeretne válaszolni az üzenetre";
+$lang['kalkun_forward'] = "Továbbít";
+$lang['kalkun_forward_to'] = "Továbbítás neki";
+$lang['kalkun_forward_title'] = "Kattintson az üzenet továbbításához";
+$lang['kalkun_move_to'] = "Mozgatás ide";
+$lang['kalkun_select_folder'] = "Mappa kiválasztása";
+$lang['kalkun_delete'] = "Törlés";
+$lang['kalkun_delete_message_title'] = "Kattintson az üzenet törléséhez";
+$lang['kalkun_delete_folder'] = "Mappa törlése";
+$lang['kalkun_delete_folder_title'] = "Kattintson a mappa törléséhez";
+$lang['kalkun_permanently'] = "véglegesen";
+$lang['kalkun_recover'] = "Visszaállítás";
+
+$lang['kalkun_send_date'] = "Küldés dátuma";
+$lang['kalkun_sms_mode'] = "SMS mód";
+$lang['kalkun_now'] = "Most";
+$lang['kalkun_now_note'] = "Az üzenet azonnali küldése";
+$lang['kalkun_at_date_time'] = "Adott dátumon és idõben";
+$lang['kalkun_after_a_delay'] = "Késleltetés után";
+
+$lang['kalkun_person'] = "Személy";
+$lang['kalkun_group'] = "Csoport";
+$lang['kalkun_input_manually'] = "Manuális bevitel";
+
+$lang['kalkun_show_hide_option'] = "Mutat/Elrejt opció";
+$lang['kalkun_add_folder'] = "Mappa hozzáadása";
+$lang['kalkun_folder_name'] = "Mappa neve";
+$lang['kalkun_cancel'] = "Mégse";
+$lang['kalkun_save'] = "Mentés";
+$lang['kalkun_rename'] = "Átnevezés";
+$lang['kalkun_rename_folder'] = "Mappa átnevezése";
+$lang['kalkun_rename_folder_title'] = "Kattintson a mappa átnevezéséhez";
+
+$lang['kalkun_delete_folder_confirmation_header'] = "Mappa törlésének megerõsítése";
+$lang['kalkun_delete_folder_confirmation'] = "A mappa és az abban lévõ üzenetek véglegesen törlõdnek és nem állíthatóak vissza. Biztos benne?";
+
+$lang['kalkun_no_message'] = "Nincs üzenet az ön ";
+$lang['kalkun_no_message_in_folder'] = "Nincs a mappában üzenet";
+
+$lang['kalkun_delete_all_confirmation_header'] = "Összes törlése";
+$lang['kalkun_delete_all_confirmation'] = "Biztos benne? Minden beszélgetést érinteni fog!";
+
+$lang['kalkun_select_all'] = "Összes kiválasztása";
+$lang['kalkun_select_all_title'] = "Kattintson az összes üzenet kiválasztásához";
+$lang['kalkun_clear_all'] = "Összes törlés";
+$lang['kalkun_clear_all_title'] = "Kattintson a kiválasztott üzenetek törléséhez";
+$lang['kalkun_refresh'] = "Frissítés";
+$lang['kalkun_global_delete_title'] = "Kattintson a kijelölt üzenetek törléséhez";
+
+$lang['kalkun_report_spam'] = "Bejelentés SPAM-ként";
+$lang['kalkun_ham'] = "Nem SPAM";
+
+$lang['kalkun_first'] = "Elsõ";
+$lang['kalkun_last'] = "Utolsó";
+
+$lang['kalkun_smsc'] = "SMSC";
+$lang['kalkun_sms_part'] = "Rész";
+$lang['kalkun_sms_part_suffix'] = "rész üzenetek";
+$lang['kalkun_sms_validity'] = "Érvényesség";
+
+$lang['kalkun_sms_type'] = "SMS Típus";
+$lang['kalkun_sms_type_normal'] = "Normál";
+$lang['kalkun_sms_type_flash'] = "Flash";
+$lang['kalkun_sms_type_wap'] = "WAP Push Link";
+$lang['kalkun_sms_type_wap_url'] = "URL";
+
+$lang['kalkun_sms_ads_active'] = "Aktív reklámok";
+$lang['kalkun_sms_ncpr_check'] = "DND ellenõrzése";
+$lang['kalkun_insert'] = "Beszúrás";
+
+$lang['kalkun_canned'] = "Megírt válaszok";
+$lang['kalkun_canned_empty'] = "Úgy tûnik nincs egyetlen elõre megírt válasz sem. Mentse el az elsõt most";
+$lang['kalkun_canned_title'] = "Válaszok kiválasztása";
+$lang['kalkun_canned_save_new'] = "Új mentése...";
+$lang['kalkun_canned_saving'] = "Mentés...";
+$lang['kalkun_canned_confirm'] = "Biztos benne?";
+$lang['kalkun_canned_template_name'] = "Adja meg az üzenete nevét. A névnek egyedinek kell lennie.";
+$lang['kalkun_canned_template_update'] = "Biztos benne? Ez felül fogja írni az elõzõ üzenetet";
+
+// Others
+$lang['kalkun_compose_amount'] = "Mennyiség";
+$lang['kalkun_compose_send_as_flash'] = "Küldés Flash SMS-ként";
+$lang['kalkun_compose_send_as_unicode'] = "Küldés mint Unicode";
+$lang['kalkun_compose_times'] = "alkalommal";
+$lang['kalkun_compose_hour'] = "Óra";
+$lang['kalkun_compose_minutes'] = "Perc";
+$lang['kalkun_no_message_search'] = "Oops! A keresés nem egyezik egy rekorddal sem";
+$lang['tni_see_conversation'] = "Beszélgetés megtekintése";
+
+$lang['kalkun_search'] = "Keresés";
+$lang['kalkun_advanced_search'] = "Összetett keresés";
+$lang['kalkun_advanced_search_from_to'] = "Tól/ig";
+$lang['kalkun_advanced_search_query'] = "Lekérés";
+$lang['kalkun_advanced_search_on'] = "Keresés be";
+$lang['kalkun_advanced_search_on_all_folder'] = "Minden mappában";
+$lang['kalkun_advanced_search_date_from'] = "Dátumtól";
+$lang['kalkun_advanced_search_date_to'] = "Dátumig";
+$lang['kalkun_advanced_search_msg_status'] = "Státusz";
+$lang['kalkun_advanced_search_msg_status_any'] = "Minden";
+$lang['kalkun_advanced_search_paging'] = "Lapozás";
+$lang['kalkun_advanced_search_paging_per_page'] = "Oldalanként";
+$lang['kalkun_advanced_search_no_paging'] = "Ne használjon lapozást";
+
+$lang['kalkun_resend'] = "Újraküldés";
+$lang['kalkun_public_contact'] = "Nyilvános kapcsolat";
+$lang['kalkun_public_contact_set'] = "Beállítás mint nyilvános kapcsolat";
+$lang['kalkun_my_contact'] = "Saját kapcsolatok";
+$lang['kalkun_public_group'] = "Nyilvános csoportok";
+$lang['kalkun_public_group_set'] = "Beállítás mint nyilvános csoport";
+$lang['kalkun_my_group'] = "Saját csoportok";
+$lang['kalkun_statistic_title'] = "Elõzõ 7 napban felhasznált sms-ek";
+$lang['kalkun_incoming_sms'] = "Bejövõ SMS";
+$lang['kalkun_outgoing_sms'] = "Kimenõ SMS";
+$lang['kalkun_delete_all_message_now'] = "Összes üzenet törlése";
+$lang['kalkun_contact_del_title'] = "Kapcsolatok törlésének megerõsítése";
+$lang['kalkun_compose_valid_url'] = "Érvényes URL-nek kell lennie";
+$lang['kalkun_compose_import_file'] = "Importálás fájlból";
+$lang['kalkun_compose_counter_character'] = "karakter";
+$lang['kalkun_compose_counter_message'] = "üzenet";
+
+// Setting
+$lang['kalkun_setting_passwd_current'] = "Aktuális jelszó";
+$lang['kalkun_setting_passwd_forgot'] = "Elfelejtette a jelszavát?";
+$lang['kalkun_setting_passwd_new'] = "Új jelszó";
+$lang['kalkun_setting_passwd_valid_rule'] = "Legalább 6 karakter hosszúnak kell lennie";
+
+$lang['kalkun_setting_passwd_current_enter'] = "Adja meg az aktuális jelszavát";
+$lang['kalkun_setting_passwd_new_enter'] = "Írja be a jelszavát";
+
+$lang['kalkun_level_admin'] = "Adminisztrátor";
+$lang['kalkun_level_user'] = "Felhasználó";
+
+$lang['kalkun_filter_add'] = "Új szûrõ létrehozása";
+$lang['kalkun_filter_has_the_words'] = "Ilyen szavak vannak";
+
+// SMS Content/Member
+$lang['kalkun_sms_member'] = "Tag";
+$lang['kalkun_sms_total_member'] = "Összes tag";
+$lang['kalkun_sms_no_member'] = "Még nincs regisztrált tag";
+
+// tni contribution
+$lang['tni_contact_add'] = "Kapcsolat hozzáadása";
+$lang['tni_send_message'] = "Üzenet küldése";
+$lang['tni_sending_message'] = "Küldés";
+$lang['tni_folders_sms'] = "SMS mappák";
+$lang['tni_name_search'] = "Név beszúrása a kapcsolati listából";
+$lang['tni_group_del_title'] = "Csoport(ok) törlésének megerõsítése";
+$lang['tni_group_delete'] = "Törli a csoporto(ka)t?<br>A csoport(ok) összes kapcsolata törlõdni fog.";
+$lang['tni_group_del_button'] = "Igen, törölje a kiválasztott csoporto(ka)t";
+$lang['tni_group_add'] = "Csoport létrehozása";
+$lang['tni_group_name'] = "Csoport neve";
+$lang['tni_group_show_contacts'] = "Mutasd a csoport kapcsolatait";
+$lang['tni_group_field_num'] = "Mezõk száma";
+$lang['tni_group_field_string'] = "Mezõ értékei";
+$lang['tni_group_field_title'] = "Csoport SMS mezõértékei"; 
+$lang['tni_groups'] = "Csoportok";
+$lang['tni_groups_manage'] = "Csoportok kezelése";   
+$lang['tni_group_manage'] = "Csoport kezelése";  
+$lang['tni_groups_edit_contacts'] = "Kapcsolat kezelése";
+$lang['tni_group_select'] = "Csoport kiválasztása";
+$lang['tni_msglist_trash_empty'] = "Nincs üzenet a kukában";  
+$lang['tni_msg_no_conv_selected'] = "Nincs kiválasztott elem";  
+$lang['tni_pbk_no_contact_selected'] = "Nincs kiválasztott kapcsolat";  
+$lang['tni_compose_sms'] = "SMS írása";  
+$lang['tni_contacts'] = "Kapcsolatok";  
+$lang['tni_settings_saved'] = "Beállításait elmentettük...";  
+$lang['tni_settings_wrong_pword'] = "Hibás a megadott jelszó";  
+$lang['tni_settings_uname_exists'] = "Létezõ felhasználónév";  
+$lang['tni_settings_unamepass_wrong'] = "Hibás felhasználónév vagy jelszó";   
+$lang['tni_back_to'] = "Vissza";   
+$lang['tni_show_details'] = "Részletek mutatása";   
+$lang['tni_hide_details'] = "Részletek rejtése";  
+$lang['tni_search_contacts'] = "Kapcsolatok keresése";
+$lang['tni_search_sms'] =  "Üzenetek keresése";  
+$lang['tni_edit'] = "Szerkesztés";   
+$lang['tni_form_fields_required'] = "Minden ûrlapmezõ kötelezõ";   
+$lang['tni_contact_name'] = "Név";   
+$lang['tni_contact_phonenumber'] = "Telefonszám";   
+$lang['tni_compose_msgto_outbox'] = "Üzenetét áthelyeztük a Kimenõ mappába<br>és készen áll a kézbesítésre.";  
+$lang['tni_email_address'] = "Email ID";    
+$lang['tni_email_forward'] = "Email továbbítás engedélyezése";
+$lang['tni_compose_searching'] = "Keresés...";  
+$lang['tni_outbox_sent_time'] = "Kevesebb, mint egy perccel ezelõtt...";  
+$lang['tni_time_remaining'] = "hátralévõ";  
+$lang['tni_pbk_edit_contact'] = "Kapcsolat szerkesztése";  
+$lang['tni_pbk_contact_edited'] = "A kapcsolat szerkesztve";  
+$lang['tni_pbk_contact_added'] = "Kapcsolat hozzáadva";  
+$lang['tni_contact_not_found'] = "Kapcsolat nem található";  
+$lang['tni_contact_search_empty'] = "Üres kapcsolat";  
+$lang['tni_user_add'] = "Felhasználó hozzáadása/szerkesztése";  
+$lang['tni_user_addp'] = "Felhasználók hozzáadása";  
+$lang['tni_user_edit'] = "Felhasználó hozzáadása/szerkesztése";  
+$lang['tni_user_word'] = "Felhasználó";  
+$lang['tni_user_wordp'] = "Felhasználók";  
+$lang['tni_user_confirm_delete'] = "Felhasználó törlésének megerõsítése";  
+$lang['tni_user_delete_body'] = "Törli a felhasználót?<br>A felhasználó minden adatával együtt (üzenetek és kapcsolatok).";  
+$lang['tni_user_not_found'] = "Felhasználó nem található";  
+$lang['tni_user_search_empty'] = "Üres felhasználó";  
+$lang['tni_user_person_name'] = "Név";  
+$lang['tni_user_username'] = "Felhasználónév";  
+$lang['tni_user_password'] = "Jelszó";  
+$lang['tni_user_search'] = "Felhasználó keresése";  
+$lang['tni_user_conf_password'] = "Jelszó ellenõrzése";  
+$lang['tni_user_updated'] = "Felhasználó frissítve";  
+$lang['tni_user_phone_number'] = "Telefon hozzáférési szám";  
+$lang['tni_action_not_allowed'] = "Nem engedélyezett mûvelet";  
+$lang['tni_error_enter_name'] = "Név megadása";  
+$lang['tni_error_enter_password'] = "Jelszó megadása";  
+$lang['tni_error_password_nomatch'] = "Nem egyeznek a jelszavak";  
+$lang['tni_error_toshort'] = "Túl rövid";  
+$lang['tni_error_nouser_sel'] = "Nincs felhasználó kiválasztva";  
+$lang['tni_level'] = "Szint";  
+$lang['tni_delete_folder'] = "Mappa törlése";  
+$lang['tni_loading'] = "Töltés";  
+$lang['tni_status'] = "Státusz";  
+$lang['tni_connected'] = "Összekapcsolt";  
+$lang['tni_disconnected'] = "Szétkapcsolt";  
+$lang['tni_user_enter_login'] = "Kérem adja meg felhasználónevét és jelszavát";  
+$lang['tni_group_no_group'] = "Nem találhatóak csoportok, hozzon létre egyet elõször";  
+$lang['tni_group_no_selected'] = "Nincs csoport kiválasztva";  
+$lang['tni_original_msg_from'] = "Honnan";  
+$lang['tni_compose_enter_dest'] = "Kérjük adja meg a célt";  
+$lang['tni_compose_enter_msg'] = "Kérjük adja meg az üzenetet";  
+$lang['tni_compose_enter_sendate'] = "Kérjük adja meg a küldés idejét";  
+$lang['tni_compose_click_calendar'] = "Kattintson a Naptárra";  
+$lang['tni_archived_by'] = "Mutassa";  
+$lang['tni_received'] = "Fogadva";  
+$lang['tni_from'] = "Honnan";  
+$lang['tni_to'] = "Hová";  
+$lang['tni_inserted'] = "Beillesztve";  
+$lang['tni_date'] = "Dátum";  
+$lang['tni_msg_stat_fail'] = "Küldés sikertelen";  
+$lang['tni_msg_stat_oknr'] = "Küldés OK - Nincs jelentés";  
+$lang['tni_msg_stat_okwr'] = "Küldés OK - Várakozás a jelentésre";  
+$lang['tni_msg_stat_deliv'] = "Elküldve";  
+$lang['tni_msg_stat_pend'] = "Függõben";  
+$lang['tni_msg_stat_unknown'] = "Ismeretlen";  
+
+$lang['tni_set_conv_order_old'] = "Legrégebbi elõre";  
+$lang['tni_set_conv_order_new'] = "Legújabb elõre";  
+$lang['tni_set_conv_sort'] = "Üzenetek rendezése";  
+
+$lang['tni_set_data_pp'] = "Adat / Oldal";  
+$lang['tni_set_data_pp_hint'] = "Az üzenetek és a telefonkönyv lapozására lesz használva";  
+
+$lang['tni_set_perm_del'] = "Végleges törlés";  
+$lang['tni_set_perm_deloff'] = "Végleges törlés kikapcsolva";  
+$lang['tni_set_perm_deloff_hint'] = " - Elõször a szemetesbe kell helyezni";   
+$lang['tni_set_perm_delon'] = "Végleges törlés bekapcsolva";  
+
+$lang['tni_set_title'] = "Felhasználó beállítások";  
+$lang['tni_set_general'] = "Általános";  
+$lang['tni_set_personal'] = "Személyes";  
+$lang['tni_set_appearance'] = "Megjelenés";  
+
+
+$lang['tni_signature']  = "Aláírás";  
+$lang['tni_signature_off']  = "Aláírás ki";  
+$lang['tni_signature_on']  = "Aláírás be";  
+$lang['tni_signature_hint']  = "Maximum 50 karakter";  
+$lang['tni_signature_hintb']  = "Az aláírás az üzenet végére lesz helyezve";   
+$lang['tni_language']  = "Nyelv";   
+$lang['tni_settings']  = "Beállítások";  
+$lang['tni_yes']  = "Igen";   
+$lang['tni_no'] = "Nem";   
+$lang['tni_default'] = "Alapértelmezett";   
+$lang['tni_set_deliv_report'] = "Kézbesítési jelentés"; 
+
+
+/* End of file kalkun_lang.php */
+/* Location: ./application/language/english/kalkun_lang.php */
