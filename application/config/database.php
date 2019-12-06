5c4fc2cb8b02b9126083bb72830d4a010269f377
ahmad <ahmad.afandi85@gmail.com>
2019-12-06 13:08:27 +0700

    hapus yang tidak dibutuhkan

--- application/config/database.php
+++ application/config/database.php
@@ -37,11 +37,11 @@
 $active_group = "default";
 $active_record = TRUE;
 
-$db['default']['hostname'] = "127.0.0.1";
+$db['default']['hostname'] = "localhost";
 
 // MySQL
-$db['default']['username'] = "ahmad";
-$db['default']['password'] = "ahmad";
+$db['default']['username'] = "root";
+$db['default']['password'] = "password";
 $db['default']['database'] = "kalkun";
 $db['default']['dbdriver'] = "mysqli";
 
