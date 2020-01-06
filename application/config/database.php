19ba6cac76d6ae79e1b59753924ebd6b0bab6e90
pandigresik <ahmad.afandi85@gmail.com>
2020-01-06 16:36:47 +0700

    set correct defalt path plugin directory (#192)
    * set default path plugins
    * set default connection

--- application/config/database.php
+++ application/config/database.php
@@ -37,7 +37,7 @@
 $active_group = "default";
 $active_record = TRUE;
 
-$db['default']['hostname'] = "127.0.0.1";
+$db['default']['hostname'] = "localhost";
 
 // MySQL
 $db['default']['username'] = "root";
@@ -56,7 +56,7 @@ $db['default']['dbdriver'] = "mysqli";
 // $db['default']['dbdriver'] = "pdo";
 
 $db['default']['dbprefix'] = "";
-$db['default']['pconnect'] = FALSE;
+$db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = "";
