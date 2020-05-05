e3b0f7346446fe84c6166d90662393c2c68234f0
tenzap <fabstz-it@yahoo.fr>
2020-05-05 19:17:30 +0200

    stop_manager: add search feature and sort results

--- application/plugins/stop_manager/views/index.php
+++ application/plugins/stop_manager/views/index.php
@@ -1,25 +1,19 @@
 <?php $this->load->view('js_stop_manager');?>
 
-<!-- Add STOP dialog -->
-<div id="stop-dialog" title="Add STOP Number" class="dialog">
-    <p id="validateTips">All form fields are required.</p>
-    <?php echo form_open('plugin/stop_manager', array('class' => 'addstopform')); ?>
-    <fieldset>
-        <label for="destination_number">Phone Number</label>
-        <input type="text" name="destination_number" id="destination_number" class="text ui-widget-content ui-corner-all" />
-        <label for="stop_type">Type</label>
-        <input type="text" name="stop_type" id="stop_type" class="text ui-widget-content ui-corner-all" />
-        <label for="stop_message">Original opt-out SMS</label>
-        <input type="text" name="stop_message" id="stop_message" class="text ui-widget-content ui-corner-all" />
-    </fieldset>
-    <?php echo form_close(); ?>
-</div>
+<div id="window_container">
+    <div id="window_title">
+        <div id="window_title_left">Stop Manager records</div>
+        <div id="window_title_right">
+            <?php echo form_open('plugin/stop_manager', array('class' => 'search_form')); ?>
+            <input type="text" name="search_name" size="20" class="search_name" placeholder="Search..." value="<?php echo $this->input->post('search_name');?>" />
+            <?php echo form_close(); ?>
+            &nbsp;
+            <a href="<?php echo current_url();?>" class="nicebutton">Reset search</a>
+            <a href="#" id="addstopbutton" class="nicebutton">&#43; Add STOP record</a>
+        </div>
+    </div>
 
-<div id="space_area">
-<h3 style="float: left">Stop Manager Numbers</h3>
-<div style="float: right">
-<a href="#" id="addstopbutton" class="nicebutton">&#43; Add STOP number</a>
-</div>
+    <div id="window_content">
 
     <table class="nice-table" cellpadding="0" cellspacing="0">
         <tr>
@@ -34,7 +28,7 @@
         <?php
         if($stoplist->num_rows()==0)
         {
-            echo "<tr><td colspan=\"6\" style=\"border-left: 1px solid #000; border-right: 1px solid #000;\">No STOP number found.</td></tr>";
+            echo "<tr><td colspan=\"6\" style=\"border-left: 1px solid #000; border-right: 1px solid #000;\">No STOP record found.</td></tr>";
         }
         else
         {
@@ -59,9 +53,33 @@
         }
         ?>
         <tr>
-            <th colspan="6" class="nice-table-footer"><div id="simplepaging"><?php echo $this->pagination->create_links();?></div></th>
+            <th colspan="6" class="nice-table-footer">
+                <div id="simplepaging">
+                <?php if (is_null($this->input->post('search_name')))
+                    echo $this->pagination->create_links();
+                ?>
+                </div>
+            </th>
         </tr>
 
     </table>
     <br />
+    </div>
+  </div>
+</div>
+
+
+<!-- Add STOP dialog -->
+<div id="stop-dialog" title="Add STOP record" class="dialog">
+    <p id="validateTips">All form fields are required.</p>
+    <?php echo form_open('plugin/stop_manager', array('class' => 'addstopform')); ?>
+    <fieldset>
+        <label for="destination_number">Phone Number</label>
+        <input type="text" name="destination_number" id="destination_number" class="text ui-widget-content ui-corner-all" />
+        <label for="stop_type">Type</label>
+        <input type="text" name="stop_type" id="stop_type" class="text ui-widget-content ui-corner-all" />
+        <label for="stop_message">Original opt-out SMS</label>
+        <input type="text" name="stop_message" id="stop_message" class="text ui-widget-content ui-corner-all" />
+    </fieldset>
+    <?php echo form_close(); ?>
 </div>
