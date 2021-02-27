cc93e6e23c57e37946c501bc68bbdb7662c27b3b
abdullah azad <4947845+abazad@users.noreply.github.com>
2021-02-27 05:25:51 +0300

    notification.php: add color per signal level & connection status

--- application/views/main/notification.php
+++ application/views/main/notification.php
@@ -1,8 +1,8 @@
 <?php 
 $this->load->helper('kalkun');
-echo lang('tni_status').": ";
+echo "<strong>".lang('tni_status').": ";
 // Get signal and battery value
-//$signal = $this->Kalkun_model->get_gammu_info('phone_signal')->row('Signal'); 
+$signal = $this->Kalkun_model->get_gammu_info('phone_signal')->row('Signal'); 
 //$battery = $this->Kalkun_model->get_gammu_info('phone_battery')->row('Battery');
 
 $status = $this->Kalkun_model->get_gammu_info('last_activity')->row('UpdatedInDB');
@@ -12,10 +12,10 @@ if($status!=NULL) {
 	if($status=="connect") echo "<span class=\"good\">".lang('tni_connected')."</span>";
 	else echo "<span class=\"warning\">".lang('tni_disconnected')."</span>";
 }
-else echo "Unknown";
-?>
-
-<?php
-//if($status)
-//echo "Signal: ".$signal."%  Battery: ".$battery."%";
+else echo "<span class=\"yellow\">Unknown</span>";
+echo " | Signal: ";
+if ($signal >= 80) echo '<span class="good">%'.$signal.'</span>';
+elseif ($signal >= 50 AND $signal <= 79) echo '<span class="yellow">%'.$signal.'</span>';
+elseif ($signal <= 49) echo '<span class="warning">%'.$signal.'</span>';
+echo "</strong>";
 ?>
