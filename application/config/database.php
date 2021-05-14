e13960a5f906d2d6ffddac590693aced19f3ca47
JD Byrnes <204185+realJoshByrnes@users.noreply.github.com>
2021-05-15 07:06:29 +1000

    Enable full UTF8 support MySQL
    Note: The changes only affect MySQL databases, others may need further modification.
    
    Fixes back2arie/Kalkun#265

--- application/config/database.php
+++ application/config/database.php
@@ -60,8 +60,15 @@ $db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = "";
-$db['default']['char_set'] = "utf8";
-$db['default']['dbcollat'] = "utf8_general_ci";
+
+if ($db['default']['dbdriver'] == "mysqli") {
+	$db['default']['char_set'] = "utf8mb4";
+	$db['default']['dbcollat'] = "utf8mb4_general_ci";
+}
+else {
+	$db['default']['char_set'] = "utf8";
+  $db['default']['dbcollat'] = "utf8_general_ci";
+}
 
 
 /* End of file database.php */
