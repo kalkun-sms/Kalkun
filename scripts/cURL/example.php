679e7badff122b85630c011ca00d81f4f09b0e0b
tenzap <fabstz-it@yahoo.fr>
2021-12-07 17:20:01 +0100

    [AUTO: PHP-CS-Fixer] scripts directory

--- scripts/cURL/example.php
+++ scripts/cURL/example.php
@@ -1,16 +1,15 @@
 <?php
-include_once("Kalkun_API.php");
+include_once('Kalkun_API.php');
 
-$config['base_url'] = "http://localhost/kalkun/index.php/";
-$config['session_file'] = "/tmp/cookies.txt";
-$config['username'] = "username";
-$config['password'] = "password";
-$config['phone_number'] = "123456";
-$config['message'] = "Test message from API";
+$config['base_url'] = 'http://localhost/kalkun/index.php/';
+$config['session_file'] = '/tmp/cookies.txt';
+$config['username'] = 'username';
+$config['password'] = 'password';
+$config['phone_number'] = '123456';
+$config['message'] = 'Test message from API';
 
 // unicode message
 // $config['coding'] = 'unicode';
 
 $sms = new Kalkun_API($config);
 $sms->run();
-?>
--- scripts/daemon.php
+++ scripts/daemon.php
@@ -1,10 +1,13 @@
 <?php
 // Make sure it's run from CLI
-if(php_sapi_name() != 'cli' && !empty($_SERVER['REMOTE_ADDR'])) exit("Access Denied.");	
+if (php_sapi_name() != 'cli' && ! empty($_SERVER['REMOTE_ADDR']))
+{
+	exit('Access Denied.');
+}
 
 // Please configure this
-$url = "http://localhost/kalkun";
+$url = 'http://localhost/kalkun';
 
-$result = file_get_contents($url."/index.php/daemon/message_routine/", "r");
+$result = file_get_contents($url.'/index.php/daemon/message_routine/', 'r');
 
 echo $result;
