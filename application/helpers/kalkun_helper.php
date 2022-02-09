514037a74fe5c071d1723003f46316aba07a2ff6
Kinshuk Bairagi <kingster@users.noreply.github.com>
2022-02-09 18:29:48 +0530

    Update kalkun_helper.php

--- application/helpers/kalkun_helper.php
+++ application/helpers/kalkun_helper.php
@@ -6,9 +6,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * Kalkun
  * An open source web based SMS Manager
  *
- * @package.    Kalkun
- * @author.     Kalkun Dev Team
- * @license.    <https://spdx.org/licenses/GPL-3.0-or-later.html> GPL-3.0-or-later
+ * @package     Kalkun
+ * @author      Kalkun Dev Team
+ * @license     <https://spdx.org/licenses/GPL-3.0-or-later.html> GPL-3.0-or-later
  * @link        https://github.com/kalkun-sms/Kalkun/
  */
 
