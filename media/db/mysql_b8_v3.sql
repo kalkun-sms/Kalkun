24d0137ce8664ef38efc73d6d1f89abc86a9429b
tenzap <46226844+tenzap@users.noreply.github.com>
2022-02-16 15:11:20 +0100

    b8: set license on sql scripts (#285)
    And update size of token column to 255 (as it is in b8)

--- media/db/mysql_b8_v3.sql
+++ media/db/mysql_b8_v3.sql
@@ -1,3 +1,21 @@
+--   Copyright (C) 2010-2019 Tobias Leupold <tobias.leupold@gmx.de>
+-- Adapted for Kalkun
+--
+--   This file is part of the b8 package
+--
+--   This program is free software; you can redistribute it and/or modify it
+--   under the terms of the GNU Lesser General Public License as published by
+--   the Free Software Foundation in version 2.1 of the License.
+--
+--   This program is distributed in the hope that it will be useful, but
+--   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+--   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+--   License for more details.
+--
+--   You should have received a copy of the GNU Lesser General Public License
+--   along with this program; if not, write to the Free Software Foundation,
+--   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
 -- --------------------------------------------------------
 
 --
@@ -5,7 +23,7 @@
 --
 
 CREATE TABLE `b8_wordlist` (
-  `token` varchar(190) character set utf8mb4 collate utf8mb4_bin NOT NULL,
+  `token` varchar(255) character set utf8mb4 collate utf8mb4_bin NOT NULL,
   `count_ham` int unsigned default NULL,
   `count_spam` int unsigned default NULL,
   PRIMARY KEY (`token`)
