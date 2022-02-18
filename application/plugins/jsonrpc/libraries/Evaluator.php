d0ee262504a1194e405b1c46fba543f0c6952d79
tenzap <46226844+tenzap@users.noreply.github.com>
2022-02-18 10:04:43 +0100

    move Evaluator class out of Jsonrpc (#301)
    in CI3, one should have one class per file. So move Evaluator class to a new file

---
+++ application/plugins/jsonrpc/libraries/Evaluator.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Kalkun
+ * An open source web based SMS Management
+ *
+ * @package		Kalkun
+ * @author		Kalkun Dev Team
+ * @license		https://spdx.org/licenses/GPL-3.0-or-later.html
+ * @link		http://kalkun.sourceforge.net
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * Jsonrpc Class
+ *
+ * @package		Kalkun
+ * @subpackage	Plugin
+ * @category	Controllers
+ */
+
+class Evaluator implements Datto\JsonRpc\Evaluator {
+
+	public function evaluate($method, $arguments)
+	{
+		if ($method === 'sms.send_sms')
+		{
+			return self::send_sms($arguments);
+		}
+
+		throw new MethodException();
+	}
+
+	private static function send_sms($arguments)
+	{
+		if (empty($arguments))
+		{
+			throw new ArgumentException();
+		}
+
+		$CI = &get_instance();
+		$CI->load->model(array('Kalkun_model', 'Message_model'));
+
+		$data['coding'] = 'default';
+		$data['class'] = '1';
+		$data['dest'] = $arguments['phoneNumber'];
+		$data['date'] = date('Y-m-d H:i:s');
+		$data['message'] = $arguments['message'];
+		$data['delivery_report'] = 'default';
+		$data['uid'] = 1;
+		$sms = $CI->Message_model->send_messages($data);
+
+		return implode(' ', $sms);
+	}
+}
