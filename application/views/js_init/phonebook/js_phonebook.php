83926325b0058de20dd4e3e3bb9cf84a043eb86b
tenzap <fabstz-it@yahoo.fr>
2022-03-02 13:05:56 +0100

    Revert "refresh page after adding contact"
    This reverts commit 6e2604c7b7bf3612fadff0d13127bb7c7e004128.

--- application/views/js_init/phonebook/js_phonebook.php
+++ application/views/js_init/phonebook/js_phonebook.php
@@ -45,13 +45,11 @@
 											buttons: {
 												"Okay": function() {
 													$(this).dialog("close");
-													location.reload();
 												}
 											}
 										});
 										setTimeout(function() {
 											$("#contact_container").dialog('close')
-											location.reload();
 										}, 1500);
 									});
 								} else {
