cfbd88b3459c8efd74ae6b191760e5559b4e093d
tenzap <fabstz-it@yahoo.fr>
2022-03-09 17:16:43 +0100

    CSRF-prepare: messages/move_message, add csrf token to POST data (move, recover conversation)

--- application/views/js_init/message/js_function.php
+++ application/views/js_init/message/js_function.php
@@ -89,22 +89,31 @@
 				$("input.select_conversation:checked:visible").each(function() {
 					var message_row = $(this).parents('div:eq(2)');
 					$.post(move_url, {
-						<?php if ($this->config->item('conversation_grouping')): ?>
-						type: 'conversation',
-						current_folder: current_folder,
-						folder: source,
-						id_folder: id_folder,
-						number: $(this).val()
-						<?php else: ?>
-						type: 'single',
-						current_folder: current_folder,
-						folder: source,
-						id_folder: id_folder,
-						id_message: $(this).val(),
-						<?php endif; ?>
-					}, function() {
-						$(message_row).slideUp("slow");
-					});
+							[csrf_name]: csrf_hash,
+							<?php if ($this->config->item('conversation_grouping')): ?>
+							type: 'conversation',
+							current_folder: current_folder,
+							folder: source,
+							id_folder: id_folder,
+							number: $(this).val(),
+							<?php else: ?>
+							type: 'single',
+							current_folder: current_folder,
+							folder: source,
+							id_folder: id_folder,
+							id_message: $(this).val(),
+							<?php endif; ?>
+						})
+						.done(function() {
+							$(message_row).slideUp("slow");
+						})
+						.fail(function(data) {
+							display_error_container(data);
+							return;
+						})
+						.always(function(data) {
+							update_csrf_hash();
+						});
 				});
 				show_notification(notif);
 			}
@@ -132,22 +141,31 @@
 				$("input.select_conversation:checked:visible").each(function() {
 					var message_row = $(this).parents('div:eq(2)');
 					$.post(move_url, {
-						<?php if ($this->config->item('conversation_grouping')): ?>
-						type: 'conversation',
-						current_folder: current_folder,
-						folder: source,
-						id_folder: id_folder,
-						number: $(this).val()
-						<?php else: ?>
-						type: 'single',
-						current_folder: current_folder,
-						folder: source,
-						id_folder: id_folder,
-						id_message: $(this).val(),
-						<?php endif; ?>
-					}, function() {
-						$(message_row).slideUp("slow");
-					});
+							[csrf_name]: csrf_hash,
+							<?php if ($this->config->item('conversation_grouping')): ?>
+							type: 'conversation',
+							current_folder: current_folder,
+							folder: source,
+							id_folder: id_folder,
+							number: $(this).val(),
+							<?php else: ?>
+							type: 'single',
+							current_folder: current_folder,
+							folder: source,
+							id_folder: id_folder,
+							id_message: $(this).val(),
+							<?php endif; ?>
+						})
+						.done(function() {
+							$(message_row).slideUp("slow");
+						})
+						.fail(function(data) {
+							display_error_container(data);
+							return;
+						})
+						.always(function(data) {
+							update_csrf_hash();
+						});
 				});
 				show_notification(notif);
 			}
