kalkun-sms/Kalkun

commit dff2804b6110f888ec97e6b4805483e674aeeeea

load js files asynchronously (#375)
Fixes this js warning:
Synchronous XMLHttpRequest on the main thread is deprecated because of its detrimental effects to the end user’s experience. For more help http://xhr.spec.whatwg.org/

application/views/main/phonebook/contact/add_contact.php
@@ -1,9 +1,14 @@
 <!-- Contact dialog -->
-<script src="<?php echo $this->config->item('js_path');?>jquery-plugin/jquery.validate.min.js"></script>
-<script src="<?php echo $this->config->item('js_path');?>jquery-plugin/jquery.tagsinput-revisited.min.js"></script>
 <link rel="stylesheet" type="text/css" href="<?php echo $this->config->item('css_path');?>jquery-plugin/jquery.tagsinput-revisited.min.css" />
 <script type="text/javascript">
-	$(document).ready(function() {
+	$.when(
+	$.cachedScript("<?php echo $this->config->item('js_path');?>jquery-plugin/jquery.validate.min.js"),
+	$.cachedScript("<?php echo $this->config->item('js_path');?>jquery-plugin/jquery.tagsinput-revisited.min.js"),
+	$.Deferred(function(deferred) {
+		$(deferred.resolve);
+	})
+	).done(function() {
+
 		<?php
 	$group = $this->Phonebook_model->get_phonebook(array('option' => 'group'));
 	$grouptext_array = [];
@@ -63,19 +68,19 @@
 		});
 
 		$('#addContact').validate();
-	});
 
-	jQuery.validator.classRuleSettings.phone = {
-		remote: {
-			url: "<?php echo site_url('kalkun/phone_number_validation'); ?>",
-			type: "get",
-			data: {
-				phone: function() {
-					return $("#number").val();
+		jQuery.validator.classRuleSettings.phone = {
+			remote: {
+				url: "<?php echo site_url('kalkun/phone_number_validation'); ?>",
+				type: "get",
+				data: {
+					phone: function() {
+						return $("#number").val();
+					}
 				}
-			}
-		}
-	};
+			};
+		});
+	});
 
 </script>
 

application/views/js_init/js_layout.php
@@ -377,4 +377,17 @@
 		<?php endif;?>
 	});
 
+	jQuery.cachedScript = function(url, options) {
+		// Allow user to set any option except for dataType, cache, and url
+		options = $.extend(options || {}, {
+			dataType: "script",
+			cache: true,
+			url: url
+		});
+
+		// Use $.ajax() since it is more flexible than $.getScript
+		// Return the jqXHR object so we can chain callbacks
+		return jQuery.ajax(options);
+	};
+
 </script>

application/views/js_init/message/js_compose.php
@@ -1,5 +1,13 @@
 <script type="text/javascript">
-	$(document).ready(function() {
+	$.when(
+		$.cachedScript("<?php echo $this->config->item('js_path');?>jquery-plugin/jquery.validate.min.js"),
+		$.cachedScript("<?php echo $this->config->item('js_path');?>jquery-plugin/jquery.form.min.js"),
+		$.cachedScript("<?php echo $this->config->item('js_path');?>jquery-plugin/jquery.tagsinput-revisited.min.js"),
+		$.Deferred(function(deferred) {
+			$(deferred.resolve);
+		})
+	).done(function() {
+
 		var max_chars_per_sms;
 		var message_length_correction;
 

application/views/main/messages/compose.php
@@ -1,7 +1,4 @@
 <?php $this->load->helper('html');?>
-<script language="javascript" src="<?php echo $this->config->item('js_path');?>jquery-plugin/jquery.validate.min.js"></script>
-<script language="javascript" src="<?php echo $this->config->item('js_path');?>jquery-plugin/jquery.form.min.js"></script>
-<script language="javascript" src="<?php echo $this->config->item('js_path');?>jquery-plugin/jquery.tagsinput-revisited.min.js"></script>
 <link rel="stylesheet" type="text/css" href="<?php echo $this->config->item('css_path');?>jquery-plugin/jquery.tagsinput-revisited.min.css" />
 <style type="text/css">
 	.left_aligned {

commit be00220c8f1060d3b6ae78934a87cc54dd4acb93

fix js syntax error
Introduced in dff2804b6110f888ec97e6b4805483e674aeeeea

application/views/main/phonebook/contact/add_contact.php
@@ -77,9 +77,9 @@
 					phone: function() {
 						return $("#number").val();
 					}
-				}
-			};
-		});
+				},
+			},
+		};
 	});
 
 </script>