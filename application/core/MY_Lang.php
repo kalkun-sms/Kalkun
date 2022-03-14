b13090a778d5d4a9faaa29fa907b1abdcce58fbf
tenzap <fabstz-it@yahoo.fr>
2022-03-14 16:36:16 +0100

    update project homepage to  https://kalkun-sms.github.io/

--- application/core/MY_Lang.php
+++ application/core/MY_Lang.php
@@ -8,7 +8,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @copyright 2021 Fab Stz
  * @author Fab Stz <fabstz-it@yahoo.fr>
  * @license <https://spdx.org/licenses/GPL-3.0-or-later.html> GPL-3.0-or-later
- * @link https://github.com/kalkun-sms/Kalkun/
+ * @link https://kalkun-sms.github.io/
  */
 
 /**
