kalkun-sms/Kalkun

commit fb86ca2171583502392610f6fa7628601e36c11c

Escapre more HTML & JS

application/views/main/settings/general.php
@@ -21,7 +21,7 @@ foreach ($supported_regions as $region)
 {
 	$country = Locale::getDisplayRegion('-'.$region, $this->lang->locale);
 	$label = $country . ' (+' . $phoneNumberUtil->getCountryCodeForRegion($region) . ')';
-	$country_calling_codes += [$region => $label];
+	$country_calling_codes += [$region => htmlentities($label, ENT_QUOTES)];
 }
 $collator = new Collator($this->lang->locale);
 $collator->asort($country_calling_codes);

application/controllers/Kalkun.php
@@ -44,7 +44,6 @@ class Kalkun extends MY_Controller {
 		$this->load->model('Phonebook_model');
 		$data['main'] = 'main/dashboard/home';
 		$data['title'] = 'Dashboard';
-		$data['data_url'] = site_url('kalkun/get_statistic');
 		if ($this->config->item('disable_outgoing'))
 		{
 			$data['alerts'][] = tr('Outgoing SMS disabled. Contact system administrator.');

application/helpers/kalkun_helper.php
@@ -109,26 +109,14 @@ function get_modem_status($status, $tolerant)
 
 function message_preview($str, $n)
 {
-	if (strlen($str) <= $n)
+	if (mb_strlen($str) <= $n)
 	{
-		return showtags($str);
+		return $str;
 	}
 	else
 	{
-		return showtags(mb_substr($str, 0, $n - 3)).'&#8230;';
+		return mb_substr($str, 0, $n - 3).'…';
 	}
-}
-
-function showtags($msg)
-{
-	$msg = preg_replace('/</', '&lt;', $msg);
-	$msg = preg_replace('/>/', '&gt;', $msg);
-	return $msg;
-}
-
-function showmsg($msg)
-{
-	return nl2br(showtags($msg));
 }
 
 function compare_date_asc($a, $b)

application/views/js_init/js_dashboard.php
@@ -12,7 +12,7 @@
 
 	$(document).ready(function() {
 		// Load data for the chart
-		reload("<?php echo $data_url;?>");
+		reload("<?php echo site_url('kalkun/get_statistic');?>");
 
 		$('a.stats-toggle').css("color", "#454545");
 		$('a.stats-toggle').on("click", function(e) {

application/views/js_init/message/js_conversation.php
@@ -188,7 +188,16 @@
 				$('.loading_area').text(<?php echo tr_js('Loading'); ?>);
 				$('.loading_area').fadeIn("slow");
 			}
-			$('#message_holder').load("<?php echo  site_url('messages/conversation/'.$this->uri->segment(3).'/'.$this->uri->segment(4).'/'.preg_replace ('/ /', '%20', $this->uri->segment(5)).'/'.$this->uri->segment(6, 0)) ?>", function(response, status, xhr) {
+			var url = "<?php echo site_url(
+	'messages/conversation/'.
+				implode('/', [
+					$this->uri->segment(3),
+					$this->uri->segment(4),
+					$this->uri->segment(5),
+					$this->uri->segment(6, 0),
+				])
+); ?>";
+			$('#message_holder').load(url, function(response, status, xhr) {
 				if (status == "error" || xhr.status != 200) {
 					var msg = <?php echo tr_js('Network Error. <span id="retry-progress-display">Retrying in <span id="countdown-count">10</span> seconds.</span>'); ?>;
 					$('.loading_area').html('<span style="white-space: nowrap">' + msg + '</span>');

application/views/js_init/message/js_function.php
@@ -3,21 +3,21 @@
 
 	$(document).ready(function() {
 
-		var offset = "<?php echo $offset;?>";
-		var folder = "<?php echo $folder;?>";
+		var offset = <?php echo json_protect($offset);?>;
+		var folder = <?php echo json_protect($folder);?>;
 		var base_url = "<?php echo site_url();?>";
-		var source = "<?php echo $type;?>";
+		var source = <?php echo json_protect($type);?>;
 		var delete_url = base_url + '/messages/delete_messages/';
 		var move_url = base_url + '/messages/move_message/';
-		var refresh_url = base_url + '/messages/' + folder + '/' + source;
+		var refresh_url = base_url + '/messages/' + encodeURIComponent(folder) + '/' + encodeURIComponent(source);
 		var delete_folder_url = base_url + '/kalkun/delete_folder/';
 
 		if (folder == 'folder') {
 			var current_folder = '';
 			var id_folder = '';
 		} else {
-			var current_folder = "<?php echo $id_folder;?>";
-			var id_folder = "<?php echo $id_folder;?>";
+			var current_folder = <?php echo json_protect($id_folder);?>;
+			var id_folder = <?php echo json_protect($id_folder);?>;
 			refresh_url = refresh_url + '/' + id_folder;
 		}
 
@@ -309,6 +309,8 @@
 		 *
 		 */
 		$(document).on('click', '#delete-all-link', function() {
+			var url = "<?php echo site_url('messages/delete_all'); ?>";
+			url += <?php echo json_protect(strtolower($this->Kalkun_model->get_folders('name', $this->uri->segment(4))->row('name'))); ?>;
 			$("#deletealldialog").dialog({
 				closeText: <?php echo tr_js('Close'); ?>,
 				bgiframe: true,
@@ -319,7 +321,7 @@
 						$(this).dialog('close');
 					},
 					<?php echo tr_js('Delete all'); ?>: function() {
-						$.get("<?php echo site_url('messages/delete_all').'/'.strtolower($this->Kalkun_model->get_folders('name', $this->uri->segment(4))->row('name'));?>");
+						$.get(url);
 						$(this).dialog('close');
 						refresh();
 					}

application/views/main/base.php
@@ -42,11 +42,11 @@
 		<p>If you find an issue, please report it on the <a class="base_color underline_link" href="https://github.com/kalkun-sms/Kalkun/issues" target="_blank">issue page of the project</a> and add the information below:</p>
 		<p>
 			<b>* Kalkun version:</b>
-			`<?php echo $this->config->item('kalkun_version');?> [Lang: <?php echo $this->Kalkun_model->get_setting()->row('language');?>] [CountryCode: <?php echo $this->Kalkun_model->get_setting()->row('country_code');?>]`
+			`<?php echo $this->config->item('kalkun_version');?> [Lang: <?php echo htmlentities($this->Kalkun_model->get_setting()->row('language'), ENT_QUOTES);?>] [CountryCode: <?php echo htmlentities($this->Kalkun_model->get_setting()->row('country_code'), ENT_QUOTES);?>]`
 			<br /><b>* Operating system:</b>
-			`<?php echo php_uname(); ?>`
+			`<?php echo htmlentities(php_uname(), ENT_QUOTES); ?>`
 			<br /><b>* PHP Version:</b>
-			`<?php echo phpversion(); ?>`
+			`<?php echo htmlentities(phpversion(), ENT_QUOTES); ?>`
 			<br /><b>* DB Backend:</b>
 			`<?php
 				$this->load->helper('kalkun_helper');
@@ -59,19 +59,19 @@
 			<br /><b>* Browser:</b>
 			`<?php
 					$this->load->library('user_agent');
-					echo $this->agent->browser(), ' ', $this->agent->version() ; ?>`
+					echo htmlentities($this->agent->browser(), ENT_QUOTES), ' ', htmlentities($this->agent->version(), ENT_QUOTES) ; ?>`
 			<br /><b>* Plugins:</b>
 			`<?php
 					$this->load->model('Plugin_model');
 					$installed_plugins = array_column($this->Plugin_model->get_plugins()->result_array(), 'plugin_system_name');
-					echo implode(', ', $installed_plugins);
+					echo htmlentities(implode(', ', $installed_plugins), ENT_QUOTES);
 					?>`
 		</p>
 	</div>
 </div>
 
 <!-- Add Folder Dialog -->
-<div id="addfolderdialog" title="<?php echo tr('Add folder');?>" class="dialog">
+<div id="addfolderdialog" title="<?php echo htmlentities(tr('Add folder'), ENT_QUOTES);?>" class="dialog">
 	<?php
 	$this->load->helper('form');
 	echo form_open('kalkun/add_folder', array('class' => 'addfolderform'));
@@ -218,7 +218,7 @@
 </div>
 
 <!-- Advanced Search Dialog -->
-<div id="a_search_dialog" title="<?php echo tr('Advanced search');?>" class="dialog">
+<div id="a_search_dialog" title="<?php echo htmlentities(tr('Advanced search'), ENT_QUOTES);?>" class="dialog">
 	<?php
 	$this->load->helper('form');
 	echo form_open('messages/query', array('id' => 'a_search_form'));

application/views/main/header.php
@@ -20,26 +20,12 @@ $jquery_datepicker_regional = $this->lang->get_jquery_datepicker_regional(APPPAT
 $jquery_ui_i18n = FCPATH."media/js/jquery-ui/i18n/datepicker-${jquery_datepicker_regional}.js";
 if ($jquery_datepicker_regional !== '' && file_exists($jquery_ui_i18n)):
 ?>
-<script language="javascript" src="<?php echo $this->config->item('js_path');?>jquery-ui/i18n/datepicker-<?php echo $jquery_datepicker_regional; ?>.js"></script>
+<script language="javascript" src="<?php echo $this->config->item('js_path');?>jquery-ui/i18n/datepicker-<?php echo rawurlencode($jquery_datepicker_regional); ?>.js"></script>
 <?php endif; ?>
 <!--[if IE]>
   <link type="text/css" rel="stylesheet" href="<?php echo $this->config->item('css_path');?>ie-fix.css" />
 <![endif]-->
 <?php
-//background image
-list($bg_act_option, $bg_act) = explode(';', $this->Kalkun_model->get_setting()->row('bg_image'));
-if ($bg_act_option == 'true'):?>
-<style type="text/css">
-	body {
-		background-image: url('<?php echo $this->config->item('img_path').''.$bg_act;?>');
-		background-repeat: repeat-x;
-		background-attachment: fixed;
-	}
-
-</style>
-
-<?php
-endif;
 
 $this->load->view('js_init/js_layout');
 $this->load->view('js_init/js_keyboard');

commit b51cd0582e45f1d4913b8a00d5757ebe5ff35685

fix order of country codes
The order became incorrect after having added the html_entities.

We defer html_entities to after the sorting is done.

Bug was introduced with:

https://github.com/kalkun-sms/Kalkun/commit/fb86ca2171583502392610f6fa7628601e36c11c#diff-933b1574337e3265ad8292c60a3ca042665f512b87a45b9ea82817d9722bc8e4

application/views/main/settings/general.php
@@ -21,10 +21,14 @@ foreach ($supported_regions as $region)
 {
 	$country = Locale::getDisplayRegion('-'.$region, $this->lang->locale);
 	$label = $country . ' (+' . $phoneNumberUtil->getCountryCodeForRegion($region) . ')';
-	$country_calling_codes += [$region => htmlentities($label, ENT_QUOTES)];
+	$country_calling_codes += [$region => $label];
 }
 $collator = new Collator($this->lang->locale);
 $collator->asort($country_calling_codes);
+foreach ($country_calling_codes as $region => $label)
+{
+	$country_calling_codes[$region] = htmlentities($label, ENT_QUOTES);
+}
 $dial_code_act = $this->Kalkun_model->get_setting()->row('country_code');
 echo form_dropdown('dial_code', $country_calling_codes, $dial_code_act);
 ?>