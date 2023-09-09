d2b6ccc2286f34a8cea9656fb653c567dc81d174
tenzap <fabstz-it@yahoo.fr>
2023-09-09 16:13:40 +0200

    release 0.8.1

--- application/config/kalkun_settings.php
+++ application/config/kalkun_settings.php
@@ -11,9 +11,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | https://github.com/kalkun-sms/Kalkun/wiki/Developing#Version-numbering-guidelines
 |
 */
-$config['kalkun_version'] = '0.8.1-beta-2-dev';
+$config['kalkun_version'] = '0.8.1';
 $config['kalkun_codename'] = '-';
-$config['kalkun_release_date'] = '-';
+$config['kalkun_release_date'] = '2023-09-09';
 
 /*
 |--------------------------------------------------------------------------
