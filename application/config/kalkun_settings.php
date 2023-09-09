1df6cc3fc6bd5a266074b6fd4d44889d114fc0fa
tenzap <fabstz-it@yahoo.fr>
2023-09-09 16:15:23 +0200

    Bump version to 0.8.2-alpha-1-dev

--- application/config/kalkun_settings.php
+++ application/config/kalkun_settings.php
@@ -11,9 +11,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | https://github.com/kalkun-sms/Kalkun/wiki/Developing#Version-numbering-guidelines
 |
 */
-$config['kalkun_version'] = '0.8.1';
+$config['kalkun_version'] = '0.8.2-alpha-1-dev';
 $config['kalkun_codename'] = '-';
-$config['kalkun_release_date'] = '2023-09-09';
+$config['kalkun_release_date'] = '-';
 
 /*
 |--------------------------------------------------------------------------
