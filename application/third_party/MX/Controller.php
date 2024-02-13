29796d5e9032ab2e21566e4073193b2dce4332de
tenzap <fabstz-it@yahoo.fr>
2024-02-13 13:02:06 +0100

    fix deprecation error on php 8.1
    Error was:
    
    str_replace(): Passing null to parameter #1 () of type array|string is deprecated

--- application/third_party/MX/Controller.php
+++ application/third_party/MX/Controller.php
@@ -42,7 +42,7 @@ class MX_Controller
 	
 	public function __construct() 
 	{
-		$class = str_replace(CI::$APP->config->item('controller_suffix'), '', get_class($this));
+		$class = str_replace(strval(CI::$APP->config->item('controller_suffix')), '', get_class($this));
 		log_message('debug', $class." MX_Controller Initialized");
 		Modules::$registry[strtolower($class)] = $this;	
 		
