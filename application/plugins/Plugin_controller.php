5918db808d0a5e24d18ea781a9dcd1b27296bc26
alexandergotts <154987484+alexandergotts@users.noreply.github.com>
2024-02-14 15:28:34 +0100

    Update Plugin_controller.php

--- application/plugins/Plugin_controller.php
+++ application/plugins/Plugin_controller.php
@@ -43,7 +43,7 @@ class Plugin_controller extends MY_Controller {
 		$this->plugin_name = strtolower(get_class($this));
 
 		// Prevent this controller from being called directly
-		if ("Plugin_controller" === get_class($this))
+		if (__CLASS__  === get_class($this))
 		{
 			redirect(site_url('/'), 'location', 301);
 		}
