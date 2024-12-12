3ede622add167f1306aea347790ae0da3bb99969
tenzap <fabstz-it@yahoo.fr>
2024-12-12 16:47:29 +0100

    display timezone in the header

--- application/views/main/dock.php
+++ application/views/main/dock.php
@@ -12,7 +12,7 @@
 	IntlDateFormatter::FULL,
 	IntlDateFormatter::SHORT
 );
-	echo $fmt->format(time());
+	echo $fmt->format(time()) . " " . IntlTimeZone::createDefault()->getDisplayName(false, IntlTimeZone::DISPLAY_SHORT, $this->lang->locale);
 ?>
 	</div>
 
