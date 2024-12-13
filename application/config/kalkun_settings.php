5ca90bc4b6ac8e9ccbc7e413a71254b7d7e8d1e5
tenzap <fabstz-it@yahoo.fr>
2024-12-13 10:12:28 +0100

    bump version to 0.8.3-rc-2-dev

--- application/config/kalkun_settings.php
+++ application/config/kalkun_settings.php
@@ -11,9 +11,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | https://github.com/kalkun-sms/Kalkun/wiki/Developing#Version-numbering-guidelines
 |
 */
-$config['kalkun_version'] = '0.8.3-rc-1';
+$config['kalkun_version'] = '0.8.3-rc-2-dev';
 $config['kalkun_codename'] = '-';
-$config['kalkun_release_date'] = '2024-12-13';
+$config['kalkun_release_date'] = '-';
 
 /*
 |--------------------------------------------------------------------------
