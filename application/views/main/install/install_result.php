78b26ef27ed09ef08e39dd8262db34d598e47132
tenzap <fabstz-it@yahoo.fr>
2024-12-15 23:25:40 +0100

    fix code style

--- application/views/main/install/install_result.php
+++ application/views/main/install/install_result.php
@@ -45,12 +45,12 @@
 
 <h4>Set the CodeIgniter environment variable</h4>
 <p>
-<?php if ( ! isset($_SERVER['CI_ENV']) || (isset($_SERVER['CI_ENV']) && $_SERVER['CI_ENV'] !== 'production')): ?>
-To improve security, it's recommended to set the CI_ENV variable in the configuration of your web server to <code>production</code>.
-<?php elseif (isset($_SERVER['CI_ENV']) && $_SERVER['CI_ENV'] === 'production'): ?>
-In the configuration of your web server, the CI_ENV variable is set to <code>production</code>.
-<?php endif; ?>
-This configuration is likely set in the <code><?php echo $htaccess_location; ?></code> file, or in the main configuration of your HTTP server. When set to <code>production</code>, no errors will be reported. While if set to <code>development</code>, the errors will be reported. However this is a security risk. See more details in the <a href="https://codeigniter.com/userguide3/general/environments.html">CodeIgniter documentation</a>.</p>
+	<?php if ( ! isset($_SERVER['CI_ENV']) || (isset($_SERVER['CI_ENV']) && $_SERVER['CI_ENV'] !== 'production')): ?>
+	To improve security, it's recommended to set the CI_ENV variable in the configuration of your web server to <code>production</code>.
+	<?php elseif (isset($_SERVER['CI_ENV']) && $_SERVER['CI_ENV'] === 'production'): ?>
+	In the configuration of your web server, the CI_ENV variable is set to <code>production</code>.
+	<?php endif; ?>
+	This configuration is likely set in the <code><?php echo $htaccess_location; ?></code> file, or in the main configuration of your HTTP server. When set to <code>production</code>, no errors will be reported. While if set to <code>development</code>, the errors will be reported. However this is a security risk. See more details in the <a href="https://codeigniter.com/userguide3/general/environments.html">CodeIgniter documentation</a>.</p>
 
 <h4>Configure kalkun internals</h4>
 <p>You may change some parameters in the <code><?php echo realpath(APPPATH.'config/kalkun_settings.php'); ?></code> file. For example set another gateway engine than Gammu. Find some suggestions of parameters you can change on the <a href="https://github.com/kalkun-sms/Kalkun/wiki/Configuration" target="_blank"><strong>configuration page of the wiki</strong></a>.</p>
