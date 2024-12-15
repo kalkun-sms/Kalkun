c22077dd973efc2a77f5efe02ca3bb7c54e52430
tenzap <fabstz-it@yahoo.fr>
2024-12-15 16:31:43 +0100

    exclude install/* from csrf
    CSRF is not so important for the install screens. However having it enabled
    causes trouble when refreshing the pages, which is very handy to check
    the advancement of the fulfillment of the requirements.

--- application/config/config.php
+++ application/config/config.php
@@ -483,7 +483,10 @@ $config['csrf_token_name'] = 'kalkun_csrf_tkn';
 $config['csrf_cookie_name'] = 'kalkun_csrf_cookie';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = TRUE;
-$config['csrf_exclude_uris'] = array();
+$config['csrf_exclude_uris'] = array(
+    "install/requirement_check",
+    "install/database_setup",
+    "install/run_install");
 
 /*
 |--------------------------------------------------------------------------
