56ea3a1f2e962b2d6db0d66ee9c2b7ed74d94639
tenzap <fabstz-it@yahoo.fr>
2025-02-01 19:18:54 +0100

    fix counter for 'my_folder' on dashboard/statistics

--- application/views/main/dashboard/statistic.php
+++ application/views/main/dashboard/statistic.php
@@ -101,8 +101,8 @@ $trash = $trash_inbox + $trash_sentitems;
 	<h4><?php echo tr('My folders');?>: </h4>
 	<?php
 foreach ($this->Kalkun_model->get_folders('all')->result() as $val):
-$folder_count_inbox = $this->Message_model->get_messages(array('type' => 'inbox', 'id_folder' => $val->id_folder))->num_rows();
-$folder_count_sentitems = $this->Message_model->get_messages(array('type' => 'sentitems', 'id_folder' => $val->id_folder))->num_rows();
+$folder_count_inbox = $this->Message_model->get_messages(array('type' => 'inbox', 'id_folder' => $val->id_folder, 'uid' => $this->session->userdata('id_user')))->num_rows();
+$folder_count_sentitems = $this->Message_model->get_messages(array('type' => 'sentitems', 'id_folder' => $val->id_folder, 'uid' => $this->session->userdata('id_user')))->num_rows();
 $folder_count = $folder_count_inbox + $folder_count_sentitems;
 echo '<p><span>'.htmlentities($val->name, ENT_QUOTES).': </span>'.$folder_count.'</p>';
 endforeach;
