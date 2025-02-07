6932bc75963ed423528dc6efa831aaf7f4e53c1a
tenzap <fabstz-it@yahoo.fr>
2025-02-07 19:34:47 +0100

    use trim instead of preg_replace to set base_url, and prepend /
    Useful when invoked from cli with vendor/bin/phpunit, in that case the url would be
    http://localhost/vendor... instead of http://localhostvendor/...

--- application/config/config.php
+++ application/config/config.php
@@ -27,7 +27,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $protocol = ((isset($_SERVER['HTTPS']) && ! empty($_SERVER['HTTPS'])) ? 'https' : 'http');
 $domain = array_key_exists('HTTP_HOST', $_SERVER) ? $_SERVER['HTTP_HOST'] : 'localhost';
 $config['base_url'] = $protocol. '://'.$domain ;
-$config['base_url'] .= preg_replace('@/+$@', '', dirname($_SERVER['SCRIPT_NAME'])).'/';
+$config['base_url'] .= '/'.trim(dirname($_SERVER['SCRIPT_NAME']), '/').'/';
 
 $config['css_path'] = $config['base_url'].'media/css/';
 $config['js_path']  = $config['base_url'].'media/js/';
