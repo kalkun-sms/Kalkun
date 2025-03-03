6e9f710c2a4555d75218f5475a785aa02a0db5f1
tenzap <fabstz-it@yahoo.fr>
2025-03-03 08:30:39 +0100

    tests/controllers/Install_test.php - FIRST ADDITION (contains DBSetup.php, ConfigFile.php, TestSuiteFile.php)
    Install_test: add tests for each function of the controller (contains DBSetup.php)
    
    + use credentials from DBVars.php

---
+++ tests/testutils/ConfigFile.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Kalkun
+ * An open source web based SMS Management
+ *
+ * @package		Kalkun
+ * @author		Kalkun Dev Team
+ * @license		https://spdx.org/licenses/GPL-2.0-or-later.html
+ * @link		https://kalkun.sourceforge.io/
+ */
+
+require_once __DIR__.'/TestSuiteFile.php';
+
+class ConfigFile extends TestSuiteFile {
+
+}
