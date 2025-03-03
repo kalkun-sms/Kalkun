0c6e20cd76ab36560bd8746018d9e6f356f88144
tenzap <fabstz-it@yahoo.fr>
2025-03-03 08:30:40 +0100

    HTML5: application/views/main/search.php: use CSS to format table

--- application/views/main/search.php
+++ application/views/main/search.php
@@ -2,8 +2,8 @@
 $this->load->helper('form');
 if ($this->uri->segment(1) === 'phonebook') :
 echo form_open('phonebook', array('class' => 'sms_search_form')); ?>
-<table border="0" cellpadding="0" cellspacing="0">
-	<tr valign="top">
+<table>
+	<tr style="vertical-align: top">
 		<td><input type="text" name="search_name" id="search" value="<?php if (isset($search_string))
 {
 	echo htmlentities($search_string, ENT_QUOTES);
@@ -14,14 +14,14 @@ echo form_open('phonebook', array('class' => 'sms_search_form')); ?>
 <?php echo form_close();
 else:
 echo form_open('messages/query', array('class' => 'sms_search_form')); ?>
-<table border="0" cellpadding="0" cellspacing="0">
-	<tr valign="top">
+<table>
+	<tr style="vertical-align: top">
 		<td><input type="text" name="search_sms" id="search" value="<?php if (isset($search_string))
 {
 	echo htmlentities(urldecode($search_string), ENT_QUOTES);
 }?>" class="ui-corner-left" /></td>
 		<td><input type="submit" value="<?php echo tr('Search messages');?>" /></td>
-		<td valign="middle">
+		<td style="vertical-align: middle">
 			<div style="margin-left: 5px"><small><a style="text-decoration: underline" id="a_search" href="javascript:void(0);"><?php echo tr('Advanced search');?></a></small></div>
 		</td>
 	</tr>
