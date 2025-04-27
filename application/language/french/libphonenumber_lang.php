c73a033705b236629143b35b4120a08c7960d544
tenzap <fabstz-it@yahoo.fr>
2025-04-27 10:16:05 +0200

    Add translations for \libphonenumber\NumberParseException messages

---
+++ application/language/french/libphonenumber_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['The string supplied was too long to parse.']['NumberParseException'] = 'La chaîne renseignée est trop longue à analyser';
+$lang['The string supplied did not seem to be a phone number.']['NumberParseException'] = 'La chaîne renseignée ne semble pas être un numéro de téléphone.';
+$lang['Missing or invalid default region.']['NumberParseException'] = 'Région par défaut manquante ou invalide.';
+$lang['Could not interpret numbers after plus-sign.']['NumberParseException'] = 'N\'a pas pu interpréter les chiffres après le signe plus.';
+$lang['The string supplied is too short to be a phone number.']['NumberParseException'] = 'La chaîne renseignée est trop courte pour être un numéro de téléphone.';
+$lang['The string supplied is too long to be a phone number.']['NumberParseException'] = 'La chaîne renseignée est trop longue pour être un numéro de téléphone.';
+$lang['The phone-context valid is invalid.']['NumberParseException'] = 'La valeur \'phone-context\' est invalide.';
+$lang['Phone number had an IDD, but after this was not long enough to be a viable phone number.']['NumberParseException'] = 'Le numéro a un indicatif téléphonique international, mais la suite n\'était pas assez longue pour être un numéro de téléphone viable.';
+$lang['Country calling code supplied was not recognised.']['NumberParseException'] = 'L\'indicatif téléphonique international renseigné n\'a pas été reconnu.';
+$lang['The phone-context value is invalid.']['NumberParseException'] = 'La valeur \'phone-context\' est invalide.';
