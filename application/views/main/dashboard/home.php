a8e33506c11696a6617ef4929678d77582580bce
tenzap <fabstz-it@yahoo.fr>
2026-05-08 14:13:55 +0200

    Move ENT_QUOTES inside htmlentities()

--- application/views/main/dashboard/home.php
+++ application/views/main/dashboard/home.php
@@ -31,17 +31,17 @@ endforeach;
 			<tr>
 				<td><?php echo tr('Gammu version');?></td>
 				<td>:</td>
-				<td><?php echo  filter_data(htmlentities($this->Kalkun_model->get_gammu_info('gammu_version')->row('Client') !== NULL ? $this->Kalkun_model->get_gammu_info('gammu_version')->row('Client') : ''), ENT_QUOTES); ?></td>
+				<td><?php echo  filter_data(htmlentities($this->Kalkun_model->get_gammu_info('gammu_version')->row('Client') !== NULL ? $this->Kalkun_model->get_gammu_info('gammu_version')->row('Client') : '', ENT_QUOTES)); ?></td>
 			</tr>
 			<tr>
 				<td><?php echo tr('Gammu DB schema');?></td>
 				<td>:</td>
-				<td><?php echo  filter_data(htmlentities($this->Kalkun_model->get_gammu_info('db_version')->row('Version')), ENT_QUOTES); ?></td>
+				<td><?php echo  filter_data(htmlentities($this->Kalkun_model->get_gammu_info('db_version')->row('Version'), ENT_QUOTES)); ?></td>
 			</tr>
 			<tr>
 				<td><?php echo tr('Modem IMEI');?></td>
 				<td>:</td>
-				<td><?php echo  filter_data(htmlentities($this->Kalkun_model->get_gammu_info('phone_imei')->row('IMEI') !== NULL ? $this->Kalkun_model->get_gammu_info('phone_imei')->row('IMEI') : ''), ENT_QUOTES); ?></td>
+				<td><?php echo  filter_data(htmlentities($this->Kalkun_model->get_gammu_info('phone_imei')->row('IMEI') !== NULL ? $this->Kalkun_model->get_gammu_info('phone_imei')->row('IMEI') : '', ENT_QUOTES)); ?></td>
 			</tr>
 		</table>
 	</div>
